22d30ba32261587cca55e409396afa21f5997c25
Ying <cnfty786@gmail.com>
2023-04-10 14:44:49 +0800

    fix: Title blocked by heading when scroll to title

--- src/globalStyle.tsx
+++ src/globalStyle.tsx
@@ -2,12 +2,16 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   * {
-  box-sizing: border-box;
+    box-sizing: border-box;
   }
 
   body {
     margin: 0;
     font-family: Inter;
+  }
+
+  html{
+    scroll-padding-top: 68px;
   }
 `
 
