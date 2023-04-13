bdc496c1cb1132872acd9742d3bacc2fcfb09c00
Ying <cnfty786@gmail.com>
2023-04-13 13:13:10 +0800

    feat: add file path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import vitePluginMDFormat from './plugin/vite-plugin-md-format'
+import { resolve } from 'path'
+
+const srcPath = resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/datenlord/',
+  resolve: {
+    alias: {
+      '@/': `${srcPath}/`,
+    },
+  },
   plugins: [vitePluginMDFormat(), react()],
 })
