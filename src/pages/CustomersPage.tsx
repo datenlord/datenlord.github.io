2dbc40fc926f1468ec62c560349cc517aeaa3676
Ying <cnfty786@gmail.com>
2023-05-12 02:24:12 +0800

    feat(customers-page): perfect first view

--- src/pages/CustomersPage.tsx
+++ src/pages/CustomersPage.tsx
@@ -1,9 +1,11 @@
+import { useState, useEffect, useRef } from 'react'
 import styled from 'styled-components'
 import { Header } from '@/components/Header'
 import image1Url from '@/assets/Customers/image1.svg'
 import image2Url from '@/assets/Customers/image2.svg'
 import image3Url from '@/assets/Customers/image3.svg'
 import image4Url from '@/assets/Customers/image4.png'
+import bg1Url from '@/assets/Customers/bg1.png'
 
 const ScrollContainer = styled.main`
   height: 100vh;
@@ -27,6 +29,10 @@ const ViewWrapperOdd = styled(ViewWrapper)`
 const ViewWrapperEven = styled(ViewWrapper)`
   background: #ffffff;
 `
+const ViewWrapperFirst = styled(ViewWrapper)`
+  background-image: url(${bg1Url});
+  background-size: cover;
+`
 const ViewContainer = styled.div`
   display: flex;
   flex-direction: row;
@@ -38,6 +44,10 @@ const ViewContainer = styled.div`
   padding-bottom: 0.64rem;
   padding-inline: 0.96rem;
   overflow: hidden;
+`
+const ViewContainerFirst = styled(ViewContainer)`
+  flex-direction: column;
+  text-align: center;
 `
 const Text = styled.div`
   font-weight: 500;
@@ -59,12 +69,60 @@ const ImageLeft = styled(Image)`
 `
 
 const CustomersPage: React.FC = () => {
+  const [headerTheme, setHeaderTheme] = useState<'dark' | 'light'>('dark')
+  const viewElementsRef = useRef<{
+    [propName: string]: IntersectionObserverEntry
+  }>({})
+
+  useEffect(() => {
+    const callback = (views: IntersectionObserverEntry[]) => {
+      /**
+       *  使用 useRef hook 存储被观察对象
+       */
+      viewElementsRef.current = views.reduce((map, viewElement) => {
+        map[viewElement.target.id] = viewElement
+        return map
+      }, viewElementsRef.current)
+      /**
+       *  获得所有可见标题列表
+       */
+      const visibleHeadings: IntersectionObserverEntry[] = []
+      Object.keys(viewElementsRef.current).forEach(key => {
+        const headingElement = viewElementsRef.current[key]
+        if (headingElement.isIntersecting) visibleHeadings.push(headingElement)
+        getHeaderTheme(visibleHeadings[0]?.target.id)
+      })
+    }
+    const option = {
+      rootMargin: '-40% 0px -40% 0px',
+    }
+    const observer = new IntersectionObserver(callback, option)
+    const headingElements = Array.from(document.querySelectorAll('.view'))
+    headingElements.forEach(element => observer.observe(element))
+  }, [])
+
+  const getHeaderTheme = (viewId: string) => {
+    // const viewIdArr = (viewId || '').split('-')
+    // const _headerTheme = viewIdArr[viewIdArr.length - 1]
+    switch (viewId) {
+      case 'dark':
+        setHeaderTheme('light')
+        break
+      case 'light':
+        setHeaderTheme('dark')
+        break
+      default:
+        setHeaderTheme('dark')
+        break
+    }
+  }
   return (
     <ScrollContainer>
-      <Header theme="dark" bg="transparent" activeId="customers" />
-      <ViewWrapperOdd>
-        <ViewContainer>
-          <Text>
+      <Header theme={headerTheme} bg="transparent" activeId="customers" />
+      <ViewWrapperFirst>
+        <ViewContainerFirst id="dark" className="view">
+          <Image src={image4Url} />
+          <Text style={{ color: '#D9DBEF' }}>
             By achieving high-speed access to data across clouds and data
             centers, DatenLord will greatly enhance the scalability of storage
             systems, simplify enterprise level IT business systems in terms of
@@ -74,11 +132,10 @@ const CustomersPage: React.FC = () => {
             diversified industries, such as the Internet, finance,
             telecommunications, and energy.
           </Text>
-          <ImageRight src={image4Url} />
-        </ViewContainer>
-      </ViewWrapperOdd>
+        </ViewContainerFirst>
+      </ViewWrapperFirst>
       <ViewWrapperEven>
-        <ViewContainer>
+        <ViewContainer className="view">
           <ImageLeft src={image1Url} />
           <Text>
             Cross-cloud data backup in real time, especially for key business
@@ -87,7 +144,7 @@ const CustomersPage: React.FC = () => {
         </ViewContainer>
       </ViewWrapperEven>
       <ViewWrapperOdd>
-        <ViewContainer>
+        <ViewContainer className="view">
           <Text>
             Cross-cloud data loading, tailored for artificial intelligence, and
             big data training scenarios.
@@ -96,7 +153,7 @@ const CustomersPage: React.FC = () => {
         </ViewContainer>
       </ViewWrapperOdd>
       <ViewWrapperEven>
-        <ViewContainer>
+        <ViewContainer className="view">
           <ImageLeft src={image3Url} />
           <Text>
             Cross-cloud application migration, especially for scenarios with
