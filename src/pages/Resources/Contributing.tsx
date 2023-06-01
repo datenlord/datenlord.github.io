cb1f9ce443742c53e915dc7651e5c5adb91de0e1
LingKa <cnfty786@gmail.com>
2023-06-01 14:52:35 +0800

    feat(contributing): add mobile adaptation

--- src/pages/Resources/Contributing.tsx
+++ src/pages/Resources/Contributing.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { Header } from '@/components/Header'
+import { Header } from '@/components/Header1'
 import imageUrl from '@/assets/Home/cover5.svg'
 import gitterUrl from '@/assets/gitter.svg'
 import githubUrl from '@/assets/github.svg'
@@ -24,6 +24,12 @@ const ViewWrapper = styled.div`
   height: 100vh;
   padding-top: 84px;
   color: #42424a;
+  @media screen and (max-width: 1024px) {
+    padding-top: 69px;
+  }
+  @media screen and (max-width: 768px) {
+    padding-top: 53px;
+  }
 `
 const ViewContainer = styled.div`
   align-items: center;
@@ -32,82 +38,136 @@ const ViewContainer = styled.div`
   padding-block: 64px;
   padding-inline: 128px;
   overflow: hidden;
+  @media screen and (max-width: 1024px) {
+    padding-block: 48px;
+    padding-inline: 96px;
+  }
+  @media screen and (max-width: 768px) {
+    padding-block: 32px;
+    padding-inline: 64px;
+  }
 `
 const FirstSection = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 96px;
-
   @media screen and (max-width: 1024px) {
     flex-direction: column;
+    margin-bottom: 72px;
+  }
+  @media screen and (max-width: 768px) {
+    margin-bottom: 48px;
   }
 `
 const SecondSection = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 96px;
-
   @media screen and (max-width: 1024px) {
     flex-direction: column;
     align-items: center;
-    margin-bottom: 64;
+    margin-bottom: 72px;
+  }
+  @media screen and (max-width: 768px) {
+    margin-bottom: 48px;
   }
 `
 const ThirdSection = styled.div``
 const FirstSectionContent = styled.div`
   display: flex;
   flex-direction: column;
-
   @media screen and (max-width: 1024px) {
     align-items: center;
   }
 `
 const Image = styled.img`
   margin-right: 64px;
-  width: 300px;
-
+  width: 100%;
   @media screen and (max-width: 1024px) {
-    width: 400px;
-    margin-bottom: 64px;
+    margin-bottom: 48px;
+    margin-right: 0;
+  }
+  @media screen and (max-width: 768px) {
+    margin-bottom: 32px;
+    margin-right: 0;
   }
 `
 const Heading1 = styled.div`
   margin-bottom: 32px;
   font-weight: 700;
   font-size: 30px;
-  line-height: 33px;
+  line-height: 1.1;
+  @media screen and (max-width: 1024px) {
+    margin-bottom: 24px;
+    font-size: 22.5px;
+  }
+  @media screen and (max-width: 768px) {
+    margin-bottom: 16px;
+    font-size: 15px;
+  }
 `
 const StyleHeading1 = styled(Heading1)`
   padding-left: 24px;
   border-left: 24px solid #7680dd;
   margin-bottom: 48px;
+  @media screen and (max-width: 1024px) {
+    padding-left: 18px;
+    border-width: 18px;
+    margin-bottom: 36px;
+  }
+  @media screen and (max-width: 768px) {
+    padding-left: 12px;
+    border-width: 12px;
+    margin-bottom: 24px;
+  }
 `
 const Paragraph1 = styled.div`
   font-weight: 400;
   font-size: 15px;
-  line-height: 22.2px;
+  line-height: 1.5;
+  @media screen and (max-width: 1024px) {
+    font-size: 12px;
+  }
 `
 const Paragraph2 = styled.div`
   font-weight: 400;
   font-size: 13.5px;
-  line-height: 22.2px;
+  line-height: 1.65;
   margin-bottom: 27px;
+  @media screen and (max-width: 1024px) {
+    font-size: 12;
+    margin-bottom: 20.25px;
+  }
+  @media screen and (max-width: 768px) {
+    margin-bottom: 13.5px;
+  }
 `
 const Card = styled.div`
   padding: 28px;
   width: 48%;
   border-radius: 12px;
   box-shadow: 2px 5px 15px rgba(0, 0, 0, 0.15);
-
   @media screen and (max-width: 1024px) {
     width: 100%;
     margin-bottom: 32px;
+    padding: 21px;
+    border-radius: 9px;
+  }
+  @media screen and (max-width: 768px) {
+    padding: 14px;
+    border-radius: 6px;
   }
 `
 const CardTitleContainer = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 27px;
+  @media screen and (max-width: 1024px) {
+    margin-bottom: 20.25px;
+  }
+  @media screen and (max-width: 768px) {
+    margin-bottom: 13.5px;
+  }
 `
 const CardTitle = styled.div`
   height: 100%;
@@ -115,11 +175,29 @@ const CardTitle = styled.div`
   padding-right: 16px;
   font-weight: 600;
   font-size: 18px;
-  line-height: 16.5px;
+  line-height: 0.9;
   border-right: 2px solid #000;
+  @media screen and (max-width: 1024px) {
+    margin-right: 12px;
+    padding-right: 12px;
+    font-size: 13.5px;
+    border-width: 1.5px;
+  }
+  @media screen and (max-width: 768px) {
+    margin-right: 8px;
+    padding-right: 8px;
+    font-size: 12px;
+    border-width: 1px;
+  }
 `
 const CardIcon = styled.img`
   width: 14px;
+  @media screen and (max-width: 1024px) {
+    width: 10.5px;
+  }
+  @media screen and (max-width: 768px) {
+    width: 7px;
+  }
 `
 const Button = styled.div`
   width: fit-content;
@@ -129,9 +207,20 @@ const Button = styled.div`
   color: white;
   font-weight: 400;
   font-size: 13.5px;
-  line-height: 22.2px;
+  line-height: 1.66;
   border-radius: 46.5px;
   cursor: pointer;
+  @media screen and (max-width: 1024px) {
+    margin-right: 12px;
+    padding: 3px 24px;
+    border-radius: 34.9px;
+  }
+  @media screen and (max-width: 768px) {
+    margin-right: 8px;
+    padding: 2px 16px;
+    font-size: 12px;
+    border-radius: 23.25px;
+  }
 `
 const TabContainer = styled.div`
   border-radius: 16px;
@@ -140,10 +229,15 @@ const TabContainer = styled.div`
 `
 const Tab = styled.div`
   display: flex;
-  height: 100%;
+  /* height: 100%; */
   width: 100%;
-  //
   height: 80px;
+  @media screen and (max-width: 1024px) {
+    height: 60px;
+  }
+  @media screen and (max-width: 768px) {
+    height: 40px;
+  }
 `
 const TabItem = styled.div<TabItemProps>`
   display: flex;
@@ -153,32 +247,53 @@ const TabItem = styled.div<TabItemProps>`
   color: ${({ isActive }) => (isActive ? 'white' : 'inherit')};
   font-weight: 600;
   font-size: 22.5px;
-  line-height: 33.3px;
+  line-height: 1.48;
   background: ${({ isActive }) => (isActive ? '#7680DD' : 'white')};
   border-top-left-radius: 14px;
   border-top-right-radius: 14px;
   cursor: pointer;
+  @media screen and (max-width: 1024px) {
+    font-size: 16.875px;
+    border-top-left-radius: 10.5;
+    border-top-right-radius: 10.5px;
+  }
+  @media screen and (max-width: 768px) {
+    font-size: 11.25px;
+    border-top-left-radius: 7px;
+    border-top-right-radius: 7px;
+  }
 `
 const TabPanel = styled.div`
   padding: 64px;
   background: #d9dbef;
+  @media screen and (max-width: 1024px) {
+    padding: 48px;
+  }
+  @media screen and (max-width: 768px) {
+    padding: 32px;
+  }
 `
 const TabPanelContent = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 32px;
-
   @media screen and (max-width: 1024px) {
     flex-direction: column;
     align-items: center;
+    margin-bottom: 24px;
+  }
+  @media screen and (max-width: 768px) {
+    margin: 16px;
   }
 `
 const TabPanelText = styled(Paragraph1)`
   width: 46%;
-
   @media screen and (max-width: 1024px) {
     width: 100%;
     margin-bottom: 32px;
+  }
+  @media screen and (max-width: 768px) {
+    margin-bottom: 24px;
   }
 `
 const HengyuWang = {
