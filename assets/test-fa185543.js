8c6c702b5850e00b57472c5971c8abdc3cd3e5ec
LingKa <cnfty786@gmail.com>
2023-06-13 20:40:21 +0800

    feat: publish

---
+++ assets/test-fa185543.js
@@ -0,0 +1,59 @@
+const s="/assets/test-e2cb5366.png",e="/assets/test1-a4c78321.jpg",t=[s,e],i={title:"",description:"This is a description",cover:"./test.png",author:[{name:"Zqa",age:"23?"},{name:"Ying",age:21}],date:"Invalid date"},o=[{label:"Heading 2",level:2}],h=`<h1>Heading 4</h1>
+<p>This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.</p>
+<h1>Heading 3</h1>
+<p>This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.</p>
+<h2 id="heading-2">Heading 2</h2>
+<p>This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.</p>
+<h1>Heading 1</h1>
+<p>This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.</p>
+<p><img src="${s}" alt="image">
+<img src="${e}" alt="image"></p>
+<p>This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.<br>
+This is some text.</p>
+<pre><code class="hljs language-js"><span class="hljs-keyword">const</span> hello = <span class="hljs-string">'Hello Word'</span>
+</code></pre>`;export{t as assetURLs,h as default,i as metadata,o as toc};
