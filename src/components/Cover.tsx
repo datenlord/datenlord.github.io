18b651f326cd2f167c5a7b620d5d222d68e79633
LingKa <cnfty786@gmail.com>
2023-06-30 16:00:16 +0800

    feat(cover): add sub title

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -3,10 +3,11 @@ import styled from 'styled-components'
 import { Typography } from '@/components/Typography'
 
 const { Heading } = Typography
-const { CNHead1 } = Heading
+const { CNHead1, CNTitleLarge } = Heading
 
-const SCover = styled.section<{cover: string}>`
+const SCover = styled.section<{ cover: string }>`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   margin-top: -0.72rem;
@@ -19,14 +20,23 @@ const SCover = styled.section<{cover: string}>`
 const Title = styled(CNHead1)`
   color: #fff;
 `
+const SubTitle = styled(CNTitleLarge)`
+  max-width: 10rem;
+  padding-top: 0.5rem;
+  font-weight: 400;
+  color: #fff;
+  text-align: center;
+`
 
-export const Cover: React.FC<{ cover: string; children: string }> = ({
-  cover,
-  children,
-}) => {
+export const Cover: React.FC<{
+  cover: string
+  children: string
+  subTitle?: string
+}> = ({ cover, children, subTitle }) => {
   return (
     <SCover cover={cover}>
       <Title>{children}</Title>
+      {subTitle && <SubTitle>{subTitle}</SubTitle>}
     </SCover>
   )
 }
