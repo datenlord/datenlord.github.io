62957470311d501eccad6e42caa92ffe72796f0c
LingKa <cnfty786@gmail.com>
2023-07-10 17:08:52 +0800

    feat(spinalHDL section): add resources

---
+++ src/pages/TechShare/SpinalHDL.ts
@@ -0,0 +1,84 @@
+import cover1 from '@/assets/spinal-hdl/cover1.jpg'
+import cover2 from '@/assets/spinal-hdl/cover2.jpg'
+import cover3 from '@/assets/spinal-hdl/cover3.jpg'
+import cover4 from '@/assets/spinal-hdl/cover4.jpg'
+import cover5 from '@/assets/spinal-hdl/cover5.jpg'
+import cover6 from '@/assets/spinal-hdl/cover6.jpg'
+import cover7 from '@/assets/spinal-hdl/cover7.jpg'
+import cover8 from '@/assets/spinal-hdl/cover8.jpg'
+import cover9 from '@/assets/spinal-hdl/cover9.jpg'
+
+export const spinalHDLItems = [
+  {
+    key: 1,
+    image: cover1,
+    title: 'Utilizing SpinalHDL for Large-scale Datapath Design',
+    text: '为了让对开源硬件感兴趣的朋友进一步了解SpinalHDL，达坦科技联合SpinalHDL社区举办一场题为《SpinalHDL应用前景探索》的线上研讨会（Webinar）。本研讨会分三个讨论分主题，分别为：数据通路加速（Datapath/Accelerations）、复杂系统设计案例（Design）、硬件设计流程（Flow）。这是本场研讨会的一个workshop：Utilizing SpinalHDL for Large-scale Datapath Design，由李天瑞进行分享。',
+    date: '2022.12.20',
+    url: 'https://www.bilibili.com/video/BV1PM411m7DB/',
+  },
+  {
+    key: 2,
+    image: cover2,
+    title: 'A Network Attached Deep Learning Accelerator for FPGA Clusters',
+    text: '为了让对开源硬件感兴趣的朋友进一步了解SpinalHDL，达坦科技联合SpinalHDL社区举办一场题为《SpinalHDL应用前景探索》的线上研讨会（Webinar）。本研讨会分三个讨论分主题，分别为：数据通路加速（Datapath/Accelerations）、复杂系统设计案例（Design）、硬件设计流程（Flow）。这是本场研讨会的三个workshop：A Network Attached Deep Learning Accelerator for FPGA Clusters，由Philipp Kreowsky Justin Knapheide进行分享。',
+    date: '2022.12.20',
+    url: 'https://www.bilibili.com/video/BV1HM411m7mS/',
+  },
+  {
+    key: 3,
+    image: cover3,
+    title: 'Coding a microprogrammed protocol receiver in SpinalHDL',
+    text: '为了让对开源硬件感兴趣的朋友进一步了解SpinalHDL，达坦科技联合SpinalHDL社区举办一场题为《SpinalHDL应用前景探索》的线上研讨会（Webinar）。本研讨会分三个讨论分主题，分别为：数据通路加速（Datapath/Accelerations）、复杂系统设计案例（Design）、硬件设计流程（Flow）。这是本场研讨会的五个workshop：Coding a microprogrammed protocol receiver in SpinalHDL，由Michael Meier进行分享。',
+    date: '2022.12.20',
+    url: 'https://www.bilibili.com/video/BV1oK411z7VD/',
+  },
+  {
+    key: 4,
+    image: cover4,
+    title: 'Utilizing SpinalHDL to Accelerate Neuroscience',
+    text: '为了让对开源硬件感兴趣的朋友进一步了解SpinalHDL，达坦科技联合SpinalHDL社区举办一场题为《SpinalHDL应用前景探索》的线上研讨会（Webinar）。本研讨会分三个讨论分主题，分别为：数据通路加速（Datapath/Accelerations）、复杂系统设计案例（Design）、硬件设计流程（Flow）。这是本场研讨会的二个workshop：Utilizing SpinalHDL to Accelerate Neuroscience，由Jeff-Ciesielski进行分享。',
+    date: '2022.12.20',
+    url: 'https://www.bilibili.com/video/BV1GG4y1J7gU/',
+  },
+  {
+    key: 5,
+    image: cover5,
+    title: 'SpinalHDL in Computer Architecture Research',
+    text: '为了让对开源硬件感兴趣的朋友进一步了解SpinalHDL，达坦科技联合SpinalHDL社区举办一场题为《SpinalHDL应用前景探索》的线上研讨会（Webinar）。本研讨会分三个讨论分主题，分别为：数据通路加速（Datapath/Accelerations）、复杂系统设计案例（Design）、硬件设计流程（Flow）。这是本场研讨会的四个workshop：SpinalHDL in Computer Architecture Research，由Sallar Ahmadi-Pour ( saahm)进行分享。',
+    date: '2022.12.20',
+    url: 'https://www.bilibili.com/video/BV1gA411X7f8/',
+  },
+  {
+    key: 6,
+    image: cover6,
+    title: 'NaxRiscv: A Pipeline/Plugins/SpinalHDL/Scala mix',
+    text: '为了让对开源硬件感兴趣的朋友进一步了解SpinalHDL，达坦科技联合SpinalHDL社区举办一场题为《SpinalHDL应用前景探索》的线上研讨会（Webinar）。本研讨会分三个讨论分主题，分别为：数据通路加速（Datapath/Accelerations）、复杂系统设计案例（Design）、硬件设计流程（Flow）。这是本场研讨会的第六个workshop：NaxRiscv: A Pipeline/Plugins/SpinalHDL/Scala mix，由Dolu1990进行分享。',
+    date: '2022.12.21',
+    url: 'https://www.bilibili.com/video/BV13K41167q5/',
+  },
+  {
+    key: 7,
+    image: cover7,
+    title: 'Recent progress on formal verification support on SpinalHDL',
+    text: '为了让对开源硬件感兴趣的朋友进一步了解SpinalHDL，达坦科技联合SpinalHDL社区举办一场题为《SpinalHDL应用前景探索》的线上研讨会（Webinar）。本研讨会分三个讨论分主题，分别为：数据通路加速（Datapath/Accelerations）、复杂系统设计案例（Design）、硬件设计流程（Flow）。这是本场研讨会的第七个workshop：Recent progress on formal verification support on SpinalHDL，由Xiao Yindong (Readon)进行分享。',
+    date: '2022.12.21',
+    url: 'https://www.bilibili.com/video/BV1bG411N7fk/',
+  },
+  {
+    key: 8,
+    image: cover8,
+    title: 'SpinalHDL for ASIC',
+    text: '为了让对开源硬件感兴趣的朋友进一步了解SpinalHDL，达坦科技联合SpinalHDL社区举办一场题为《SpinalHDL应用前景探索》的线上研讨会（Webinar）。本研讨会分三个讨论分主题，分别为：数据通路加速（Datapath/Accelerations）、复杂系统设计案例（Design）、硬件设计流程（Flow）。这是本场研讨会的第八个workshop：SpinalHDL for ASIC，由Sebastien-riou进行分享。',
+    date: '2022.12.21',
+    url: 'https://www.bilibili.com/video/BV16e4y1L7dV/',
+  },
+  {
+    key: 9,
+    image: cover9,
+    title: 'From Peripheral Plugins to Product documentation',
+    text: '为了让对开源硬件感兴趣的朋友进一步了解SpinalHDL，达坦科技联合SpinalHDL社区举办一场题为《SpinalHDL应用前景探索》的线上研讨会（Webinar）。本研讨会分三个讨论分主题，分别为：数据通路加速（Datapath/Accelerations）、复杂系统设计案例（Design）、硬件设计流程（Flow）。这是本场研讨会的第九个workshop：From Peripheral Plugins to Product documentation，由Andreas Wallner进行分享。',
+    date: '2022.12.21',
+    url: 'https://www.bilibili.com/video/BV12v4y1Q7cg/',
+  },
+]
