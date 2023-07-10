595bf1bd376a8ed719e599f9a1d8a05bc972b010
LingKa <cnfty786@gmail.com>
2023-07-10 17:43:19 +0800

    feat(sharing section): completed new design

--- src/pages/Resources/SharingSection.tsx
+++ src/pages/Resources/SharingSection.tsx
@@ -1,33 +1,46 @@
+import { useNavigate } from 'react-router-dom'
 import { styled } from 'styled-components'
 
 import { Typography } from '@/components/Typography'
 
-import sharingTextImageUrl from '@/assets/resources/sharing-text-image.png'
+import techTalkCover from '@/assets/resources/tech-talk-cover.png'
+import OSSShowcaseCover from '@/assets/resources/oss-showcase-cover.png'
+import rustShowcaseCover from '@/assets/resources/rust-showcase-cover.png'
+import spinalHDLCover from '@/assets/resources/spinal-hdl-cover.png'
 
 const items = [
   {
     key: 1,
-    cover: sharingTextImageUrl,
+    cover: techTalkCover,
     title: '前沿科技分享',
-    text: '描述文字描述文字描述文字描述文字',
+    text: '探索前沿技术，拥抱创新与改变',
+    url: '/tech-talk',
   },
   {
     key: 2,
-    cover: sharingTextImageUrl,
-    title: '前沿科技分享',
-    text: '描述文字描述文字描述文字描述文字',
+    cover: OSSShowcaseCover,
+    title: '开源产品分享',
+    text: '软硬件融合创新实践的观察与输出',
+    url: '/oss-showcase',
   },
   {
     key: 3,
-    cover: sharingTextImageUrl,
-    title: '前沿科技分享',
-    text: '描述文字描述文字描述文字描述文字',
+    cover: rustShowcaseCover,
+    title: 'Rust语言应用及案例分享',
+    text: 'Rust编程语言的应用、实践与探索',
+    url: '/rust-showcase',
+  },
+  {
+    key: 4,
+    cover: spinalHDLCover,
+    title: 'SpinalHDL',
+    text: 'SpinalHDL的实践、挑战与应用前景',
+    url: '/spinal-hdl',
   },
 ]
 
-const { Heading, Paragraph } = Typography
+const { Heading } = Typography
 const { CNHead4 } = Heading
-const { CNBodyLarge } = Paragraph
 
 const SectionWrapper = styled.section`
   background: ${props => props.theme.themeDark};
@@ -37,7 +50,7 @@ const SectionContainer = styled.div`
   margin-inline: auto;
   padding-top: 1rem;
   padding-bottom: 1.8rem;
-  padding-inline: 1.53rem;
+  padding-inline: 1.22rem;
 `
 const Title = styled(CNHead4)`
   padding-bottom: 1.35rem;
@@ -50,7 +63,7 @@ const Container = styled.div`
 `
 const CardWrapper = styled.div`
   display: flex;
-  width: 31%;
+  width: 24%;
   padding: 0.01rem;
   background: linear-gradient(45deg, #ffffff90, #ffffff00 60%);
   border-radius: 0.12rem;
@@ -67,25 +80,35 @@ const CardCover = styled.img`
   border-radius: 0.12rem;
 `
 const CardContent = styled.div`
-  padding-block: 0.38rem;
-  padding-inline: 0.25rem;
+  padding-block: 0.26rem;
+  padding-inline: 0.2rem;
 `
-const CardTitle = styled(CNHead4)`
+const CardTitle = styled.div`
+  padding-bottom: 0.08rem;
+  font-size: 0.226rem;
+  font-weight: 600;
   color: #fff;
 `
-const CardText = styled(CNBodyLarge)`
+const CardText = styled.div`
+  font-size: 0.145rem;
+  line-height: 0.258rem;
   color: ${props => props.theme.secondary02};
 `
 
 export default () => {
+  const navigate = useNavigate()
   return (
     <SectionWrapper>
       <SectionContainer>
         <Title>技术分享</Title>
         <Container>
-          {items.map(({ key, cover, title, text }) => (
+          {items.map(({ key, cover, title, text, url }) => (
             <CardWrapper key={key}>
-              <Card>
+              <Card
+                onClick={() => {
+                  navigate(url)
+                }}
+              >
                 <CardCover src={cover} />
                 <CardContent>
                   <CardTitle>{title}</CardTitle>
