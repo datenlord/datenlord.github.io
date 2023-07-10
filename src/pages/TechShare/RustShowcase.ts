cf1c8924e187c5d25964a14357d81f3ffce7405b
LingKa <cnfty786@gmail.com>
2023-07-10 16:38:52 +0800

    feat(rest showcase section): add resources

---
+++ src/pages/TechShare/RustShowcase.ts
@@ -0,0 +1,86 @@
+import cover1 from '@/assets/rust-showcase/cover1.jpg'
+import cover2 from '@/assets/rust-showcase/cover2.jpg'
+import cover3 from '@/assets/rust-showcase/cover3.jpg'
+import cover4 from '@/assets/rust-showcase/cover4.jpg'
+import cover5 from '@/assets/rust-showcase/cover5.jpg'
+import cover6 from '@/assets/rust-showcase/cover6.jpg'
+import cover7 from '@/assets/rust-showcase/cover7.jpg'
+import cover8 from '@/assets/rust-showcase/cover8.jpg'
+import cover9 from '@/assets/rust-showcase/cover9.jpg'
+
+export const rustShowcaseItems = [
+  {
+    key: 7,
+    image: cover7,
+    title: '2022年Rust开发者大会Workshop One：用Rust实现RDMA高性能编程',
+    text: '2022年7月31日，北京达坦科技有限公司获邀参加Rust China大会，并做公司专题workshop。这是本场workshop的第一部分，将为大家介绍如何用Rust实现RDMA高性能编程。',
+    date: '2022.08.03',
+    url: 'https://www.bilibili.com/video/BV13e4y1D7Ch/',
+  },
+  {
+    key: 8,
+    image: cover8,
+    title: '2022年Rust开发者大会Workshop Two：Async RDMA',
+    text: '2022年7月31日，北京达坦科技有限公司获邀参加Rust China大会，并做公司专题workshop。这是第二部分workshop，将为大家介绍Async RDMA。',
+    date: '2022.08.04',
+    url: 'https://www.bilibili.com/video/BV1Rh411M7EP/',
+  },
+  {
+    key: 9,
+    image: cover9,
+    title: '2022年Rust开发者大会Workshop Three：Workshop动手实验环节',
+    text: '2022年7月31日，北京达坦科技有限公司获邀参加Rust China大会，并做公司专题Workshop。这是本场Worshop的第三个部分：workshop动手环节 方便大家体验用Rust构建RDMA应用。',
+    date: '2022.08.04',
+    url: 'https://www.bilibili.com/video/BV1oN4y1V7Hn/',
+  },
+  {
+    key: 1,
+    image: cover1,
+    title: 'Rust China Hackathon2022空中路演',
+    text: '由Rust中文社区举办的题为「Rust For Fun」的首届Rust China Hackathon已经顺利完赛。达坦科技作为本届Hackathon的协办方，赞助参与本次企业组赛道。最终获得优胜奖的两支队伍在线上了举办一场空中路演。分享了：他们是如何思考拆解赛题的？在着手设计时碰到哪些难点和挑战？以及最终提交的代码设计思路和亮点。欢迎对Concurrent Indexing感兴趣的伙伴观看本条视频。',
+    date: '2023.01.15',
+    url: 'https://www.bilibili.com/video/BV1xY41197Za/',
+  },
+  {
+    key: 2,
+    image: cover2,
+    title: '4.8 Beijing Rust Meetup｜张汉东《理解 Unsafe Rust》',
+    text: '2023年4月8日，达坦科技联合南京大学、CloudWeGo、华为等技术专家成功举办了题为Rust 💡X的meetup。本场分享由《Rust 编程之道》作者、Rust 中文社区布道者张汉东老师进行，主题为《理解 Unsafe Rust》。',
+    date: '2023.04.12',
+    url: 'https://www.bilibili.com/video/BV1c24y1A7aK/',
+  },
+  {
+    key: 3,
+    image: cover3,
+    title: '4.8 Beijing Rust Meetup｜冯洋《RUFES》',
+    text: '2023年4月8日，达坦科技联合南京大学、CloudWeGo、华为等技术专家成功举办了题为Rust 💡X的meetup。本场由南京大学计算机科学与技术系助理研究员冯洋老师进行《基于实际场景应用分析的Rust库模糊测试技术报告》的演讲分享。',
+    date: '2023.04.12',
+    url: 'https://www.bilibili.com/video/BV1Qo4y1n7tS/',
+  },
+  {
+    key: 4,
+    image: cover4,
+    title: '4.8 Beijing Rust Meetup｜施继成《Rust for Linux 6.2》',
+    text: '2023年4月8日，达坦科技联合南京大学、CloudWeGo、华为等技术专家成功举办了题为Rust 💡X的meetup。本场分享由达坦科技联合创始人兼CTO施继成进行，演讲主题是《Rust for Linux 6.2》。',
+    date: '2023.04.12',
+    url: 'https://www.bilibili.com/video/BV1Bs4y117Ze/',
+  },
+  {
+    key: 5,
+    image: cover5,
+    title:
+      '4.8 Beijing Rust Meetup｜叶天星《用Rust实现Git传输协议、Git对象解析和数据库存储》',
+    text: '2023年4月8日，达坦科技联合南京大学、CloudWeGo、华为等技术专家成功举办了题为Rust 💡X的meetup。本场分享由达坦科技联合创始人兼CTO施继成进行，演讲主题是《Rust for Linux 6.2》。',
+    date: '2023.04.12',
+    url: 'https://www.bilibili.com/video/BV1Bs4y117Ze/',
+  },
+  {
+    key: 6,
+    image: cover6,
+    title:
+      '4.8 Beijing Rust Meetup｜茌海《Monoio：一个基于 io-uring 的 Rust Runtime》',
+    text: '2023年4月8日，达坦科技联合南京大学、CloudWeGo、华为等技术专家成功举办了题为Rust 💡X的meetup。本场分享由达坦科技联合创始人兼CTO施继成进行，演讲主题是《Rust for Linux 6.2》。',
+    date: '2023.04.12',
+    url: 'https://www.bilibili.com/video/BV1Rh411M7EP/',
+  },
+]
