321d3986d456943acfdd1ce67f963962a6c3c35c
LingKa <cnfty786@gmail.com>
2023-07-17 13:56:05 +0800

    feat(router): add router config

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,122 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 
-export const router = createBrowserRouter([
+import Layout from '@/pages/Layout'
+import Home from '@/pages/Home/Home'
+import Products from '@/pages/Products'
+import RDMA from '@/pages/RDMA'
+import Solutions from '@/pages/Solutions'
+import Resources1 from '@/pages/Resources/Resources1'
+import Resources2 from '@/pages/Resources/Resources2'
+import Clients from '@/pages/Clients'
+import Company1 from '@/pages/Company/Company1'
+import Company2 from '@/pages/Company/Company2'
+import VideoList from '@/pages/VideoList'
+import ArticleLIst from '@/pages/ArticleLIst'
+import ArticleContent from '@/pages/ArticleContent'
+import JDList from '@/pages/JDList'
+import JDDetail from '@/pages/JDDetail'
+
+export const router = createHashRouter([
   {
     path: '/',
-    element: <div>Vite React Template</div>,
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+      },
+      {
+        path: 'products/:sectionId',
+        element: <Products />,
+      },
+      {
+        path: 'rdma',
+        element: <RDMA />,
+      },
+      {
+        path: 'solutions',
+        element: <Solutions />,
+      },
+      {
+        path: 'solutions/:sectionId',
+        element: <Solutions />,
+      },
+      {
+        path: 'resources1',
+        element: <Resources1 />,
+      },
+      {
+        path: 'resources1/:sectionId',
+        element: <Resources1 />,
+      },
+      {
+        path: 'resources2',
+        element: <Resources2 />,
+      },
+      {
+        path: 'resources2/:sectionId',
+        element: <Resources2 />,
+      },
+      {
+        path: 'clients',
+        element: <Clients />,
+      },
+      {
+        path: 'clients/:sectionId',
+        element: <Clients />,
+      },
+      {
+        path: 'company1',
+        element: <Company1 />,
+      },
+      {
+        path: 'company1/:sectionId',
+        element: <Company1 />,
+      },
+      {
+        path: 'company2',
+        element: <Company2 />,
+      },
+      {
+        path: 'company2/:sectionId',
+        element: <Company2 />,
+      },
+      {
+        path: 'tech-talk',
+        element: <VideoList />,
+      },
+      {
+        path: 'oss-showcase',
+        element: <VideoList />,
+      },
+      {
+        path: 'rust-showcase',
+        element: <VideoList />,
+      },
+      {
+        path: 'spinal-hdl',
+        element: <VideoList />,
+      },
+      {
+        path: ':title',
+        element: <ArticleLIst />,
+      },
+      {
+        path: ':section/:title',
+        element: <ArticleContent />,
+      },
+      {
+        path: 'job-description',
+        element: <JDList />,
+      },
+      {
+        path: 'job-description/:key',
+        element: <JDDetail />,
+      },
+    ],
   },
 ])
