7f0eacb0e0ea1341669676fb84fc31cd6cc62e3a
LingKa <cnfty786@gmail.com>
2023-07-17 14:01:33 +0800

    feat: add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  padding-block: 0.12rem;
+  padding-inline: 0.34rem;
+  color: inherit;
+  background: linear-gradient(90deg, #767ee5, #9966cc);
+  border-radius: 0.5rem;
+  border: none;
+  font-size: 0.18rem;
+  line-height: 0.32rem;
+`
