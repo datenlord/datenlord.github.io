77a4e5e5592b3ab4cb62dcad97c1953229460f0b
LingKa <cnfty786@gmail.com>
2023-07-17 14:00:35 +0800

    feat(theme): add color theme

--- src/theme.ts
+++ src/theme.ts
@@ -2,10 +2,28 @@ import { DefaultTheme } from 'styled-components'
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    test: 'test'
+    themeDark: '#0A061F'
+    themeDark02: '#42424A'
+    secondary01: '#7680DD'
+    secondary02: '#D9DBEF'
+    secondary07: '#F2994A'
+    secondary08: '#FDCB6E'
+    gray03: '#828282'
+    gray04: '#4F4F4F'
+    gray007: '#272D37'
+    white00: '#fff'
   }
 }
 
 export const theme: DefaultTheme = {
-  test: 'test',
+  themeDark: '#0A061F',
+  themeDark02: '#42424A',
+  secondary01: '#7680DD',
+  secondary02: '#D9DBEF',
+  secondary07: '#F2994A',
+  secondary08: '#FDCB6E',
+  gray03: '#828282',
+  gray04: '#4F4F4F',
+  gray007: '#272D37',
+  white00: '#fff',
 }
