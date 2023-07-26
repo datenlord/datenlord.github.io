4554285ec8d1b3790aad8fba3194643d0c23075c
LingKa <cnfty786@gmail.com>
2023-07-26 22:12:47 +0800

    feat: update page

--- src/pages/Solutions.tsx
+++ src/pages/Solutions.tsx
@@ -7,14 +7,13 @@ import { Typography } from '@/components/Typography'
 import { Button } from '@/components/Button'
 
 import coverUrl from '@/assets/solutions/cover.png'
-import decorationUrl from '@/assets/solutions/decoration.png'
+import imageUrl from '@/assets/solutions/image.png'
 
 interface CardData {
   key: string
   title: string
   section1: string
   section2: string
-  decoration: boolean
 }
 
 const cardData: CardData[] = [
@@ -25,7 +24,6 @@ const cardData: CardData[] = [
       '云之间的隔阂导致数据隔离和数据碎片化。数据往往被绑定在一个特定的云计算中，无法自由访问。当业务规模较小时，云之间的隔离可能不会构成问题。然而，随着业务的发展，需要经常访问全球多个云和多个数据中心，云障碍导致的数据隔离和数据碎片化成为了业务增长的障碍。',
     section2:
       '无论数据存储在哪里，DatenLord都可以通过利用存储器来缓存热数据，从而加速跨云的数据访问，并提供统一的数据管理来实现数据的自动迁移和备份。',
-    decoration: false,
   },
   {
     key: 'metadata-management',
@@ -34,7 +32,6 @@ const cardData: CardData[] = [
       '跨云分布式存储的高延迟性和不一致性：现在的分布式共识协议只限于在单个数据中心使用，跨云分布式共识协议只限于理论研究。在跨云访问数据时，访问速度和一致性将会受到影响。',
     section2:
       '第一个产业界的基于共识协议的跨云分布式元数据管理。DatenLord利用异步编程架构，绕过了Linux内核，完全实现了独立内核、自动调度和存储IO的管理。该协议确保了跨数据节点的数据一致性。低延迟的跨云分布式共识协议保证了广域网场景下的高速和强一致性，同时保证系统中没有单点瓶颈。',
-    decoration: true,
   },
   {
     key: 'hardware-acceleration',
@@ -43,7 +40,6 @@ const cardData: CardData[] = [
       '基于软件的解决方案的性能瓶颈。跨云通信需要可靠的高速网络和快速的缓存机制。目前在大量的数据已经散布在不同的云供应商和跨云分布的数据中心的情况下，基于软件的的解决方案已经到达性能瓶颈，并且不能再满足该需求。',
     section2:
       '硬件加速器。采用硬件敏捷的开发方法来打造定制的硬件；采用RDMA和DPDK建立高性能网络；硬件实现RDMA协议、加密、压缩和编码以及超快速存储证明。',
-    decoration: false,
   },
 ]
 
@@ -59,11 +55,11 @@ const MainContainer = styled.div`
   margin-inline: auto;
   padding-top: 1rem;
   padding-bottom: 1.57rem;
-  padding-inline: 2.12rem;
+  padding-inline: 1.22rem;
 `
 const Title = styled(CNHead4)`
   text-align: center;
-  padding-bottom: 0.94rem;
+  padding-bottom: 0.58rem;
 `
 const CardWrapper = styled.div`
   position: relative;
@@ -71,20 +67,32 @@ const CardWrapper = styled.div`
   border-radius: 0.32rem;
   margin-bottom: 0.34rem;
   padding-block: 0.48rem;
-  padding-inline: 0.64rem;
+  padding-inline: 0.32rem;
+  box-shadow: 0.24rem 0.16rem 0.8rem rgba(0, 0, 0, 0.10);
   &:last-child {
     margin-bottom: 0;
   }
+  &:nth-child(odd) {
+    background: rgba(255, 255, 255, 0.2);
+  }
 `
-const Decoration1 = styled.img`
-  position: absolute;
-  top: -0.32rem;
-  right: -0.32rem;
-  width: 1.56rem;
-  height: 1.56rem;
+const CardTitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding-bottom: 0.39rem;
+`
+const DecorationLine = styled.div`
+  flex-grow: 1;
+  height: 0.01rem;
+`
+const LeftDecorationLine = styled(DecorationLine)`
+  background: linear-gradient(270deg, #926cd3 60%, #7b7ce340);
+`
+const RightDecorationLine = styled(DecorationLine)`
+  background: linear-gradient(90deg, #926cd3 60%, #7b7ce340);
 `
 const CardTitle = styled(CNHead4)`
-  padding-bottom: 0.39rem;
+  padding-inline: 0.18rem;
 `
 const Section = styled.div`
   border-radius: 0.08rem;
@@ -93,6 +101,7 @@ const Section = styled.div`
 const Section1 = styled(Section)`
   position: relative;
   max-width: 7.22rem;
+  margin-left: calc(1.54rem - 0.32rem);
   margin-bottom: 0.17rem;
   background: #9797971a;
   border-bottom-left-radius: 0;
@@ -109,6 +118,7 @@ const Decoration = styled.div`
 const Section2 = styled(Section)`
   max-width: 6rem;
   margin-left: auto;
+  margin-right: calc(1.35rem - 0.32rem);
   background: #d9dbef;
   border-bottom-right-radius: 0;
 `
@@ -129,12 +139,22 @@ const RelatedResourcesTitleEN = styled(Heading2)`
 const RelatedResourcesTitleZH = styled(CNHead4)`
   padding-bottom: 0.4rem;
 `
+const Image = styled.img`
+  display: block;
+  width: 5.36rem;
+  margin-inline: auto;
+  margin-bottom: 0.58rem;
+`
 
 const Card: React.FC<{ items: CardData }> = ({ items }) => {
-  const { key, title, section1, section2, decoration } = items
+  const { key, title, section1, section2 } = items
   return (
     <CardWrapper id={key}>
-      <CardTitle>{title}</CardTitle>
+      <CardTitleWrapper>
+        <LeftDecorationLine />
+        <CardTitle>{title}</CardTitle>
+        <RightDecorationLine />
+      </CardTitleWrapper>
       <Section1>
         <CardText>{section1}</CardText>
         <Decoration />
@@ -142,7 +162,7 @@ const Card: React.FC<{ items: CardData }> = ({ items }) => {
       <Section2>
         <CardText>{section2}</CardText>
       </Section2>
-      {decoration ? <Decoration1 src={decorationUrl} /> : null}
+      {/* {decoration ? <Decoration1 src={decorationUrl} /> : null} */}
     </CardWrapper>
   )
 }
@@ -165,6 +185,7 @@ export default () => {
       <MainWrapper>
         <MainContainer>
           <Title>解决方案</Title>
+          <Image src={imageUrl} />
           {cardData.map(items => (
             <Card key={items.key} items={items} />
           ))}
