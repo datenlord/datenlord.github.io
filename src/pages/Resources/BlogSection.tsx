e46e8eed8daf5c3154a997d6edfbaadb5d95bbc1
LingKa <cnfty786@gmail.com>
2023-07-26 18:45:26 +0800

    feat: add formal content

--- src/pages/Resources/BlogSection.tsx
+++ src/pages/Resources/BlogSection.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom'
 
@@ -7,7 +8,7 @@ import blogTestImageUrl from '@/assets/resources/blog-test-image.png'
 
 const { Heading, Paragraph } = Typography
 const { CNHead4, TitleLarge } = Heading
-const { CNBodySmall, CNBodyMedium } = Paragraph
+const { CNBodyMedium } = Paragraph
 
 const SectionWrapper = styled.section`
   background: #f7f7f9;
@@ -31,6 +32,7 @@ const MainContainer = styled.div`
 const ContentContainer = styled.div`
   padding-bottom: 0.59rem;
   display: grid;
+  grid-auto-flow: column;
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: repeat(3, 1fr);
   grid-gap: 0.36rem;
@@ -49,15 +51,26 @@ const BlogItem = styled.div`
   border-bottom: 0.01rem solid #a4a4a4;
 `
 const BlogTitle = styled(TitleLarge)`
-  padding-bottom: 0.15rem;
+  margin-bottom: 0.15rem;
   color: ${props => props.theme.gray007};
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  cursor: pointer;
+  &:hover {
+    color: #722ed1;
+  }
 `
 const BlogDescription = styled(CNBodyMedium)`
   padding-bottom: 0.07rem;
   color: #383838;
-`
-const BlogText = styled(CNBodySmall)`
-  color: #a4a4a4;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `
 const Button = styled(CNBodyMedium)`
   color: #fff;
@@ -72,53 +85,52 @@ const Button = styled(CNBodyMedium)`
   cursor: pointer;
 `
 
-const items = [
-  {
-    key: 1,
-    title: '标题标题标题',
-    description:
-      '描述文字描述文字描述文字描述文字描述文描述文字描述文字描述文字......',
-    text: '文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...',
-  },
-  {
-    key: 2,
-    title: '标题标题标题',
-    description:
-      '描述文字描述文字描述文字描述文字描述文描述文字描述文字描述文字......',
-    text: '文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...',
-  },
-  {
-    key: 3,
-    title: '标题标题标题',
-    description:
-      '描述文字描述文字描述文字描述文字描述文描述文字描述文字描述文字......',
-    text: '文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...',
-  },
-  {
-    key: 4,
-    title: '标题标题标题',
-    description:
-      '描述文字描述文字描述文字描述文字描述文描述文字描述文字描述文字......',
-    text: '文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...文章关键词关键词...',
-  },
-]
+const _blogs = import.meta.glob(`@/articles/blogs/*/index.md`)
 
 export const BlogSection: React.FC = () => {
   const navigate = useNavigate()
+  const [blogs, setBlogs] = useState<typeof import('*.md')[]>([])
+  const [cover, setCover] = useState<string>('')
+
+  const getBlogs = async () => {
+    const blogs = (await Promise.all(
+      Object.keys(_blogs).map(path => _blogs[path]()),
+    )) as typeof import('*.md')[]
+    setBlogs(blogs.reverse())
+  }
+
+  useEffect(() => {
+    getBlogs()
+  }, [])
+
+  useEffect(() => {
+    setCover(blogs[0]?.assetURLs[0] || blogTestImageUrl)
+  }, [blogs])
+
   return (
-    <SectionWrapper id='blog'>
+    <SectionWrapper id="blog">
       <SectionContainer>
         <Title>技术博客</Title>
         <MainContainer>
           <ContentContainer>
-            <Image src={blogTestImageUrl} />
-            {items.map(({ key, title, description, text }) => (
-              <BlogItem key={key}>
-                <BlogTitle>{title}</BlogTitle>
-                <BlogDescription>{description}</BlogDescription>
-                <BlogText>{text}</BlogText>
-              </BlogItem>
-            ))}
+            <Image src={cover} />
+            {blogs.map(({ metadata }, index) => {
+              const { title, date, label, description } = metadata
+              return (
+                index < 4 && (
+                  <BlogItem key={title}>
+                    <BlogTitle
+                      onClick={() => {
+                        navigate(`/blogs/${date}-${title.split(' ').join('-')}`)
+                      }}
+                    >
+                      {label}
+                    </BlogTitle>
+                    <BlogDescription>{description}</BlogDescription>
+                  </BlogItem>
+                )
+              )
+            })}
           </ContentContainer>
           <Button onClick={() => navigate('/blogs')}>了解更多</Button>
         </MainContainer>
