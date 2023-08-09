a843115eab6e31660340ded105e1ceff22c6f455
LingKa <cnfty786@gmail.com>
2023-08-10 00:38:11 +0800

    feat: deploy github page

--- zh-cn/assets/index-37f93c7b.js
+++ zh-cn/assets/index-37f93c7b.js
@@ -1,7 +1,7 @@
-const t="/zh-cn/assets/image1-d5e6eb74.jpg",e="/zh-cn/assets/image2-5d4b8e33.jpg",s="/zh-cn/assets/image3-a4b62ce6.jpg",a=[t,e,s],p={label:"创造性地精细造车轮：RDMA的Rust安全实践",description:"极客时间打造的QCon+案例研习社以案例讲解视频、主题直播、社群交互结合的全新线上学习模式，为数字化人才提供利用碎片化时间来提升知识和技能的平台",location:"新加坡",date:"2022-07-28",title:"Creatively fine tuned wheels: RDMA's Rust safety practices"},i=[{label:"讲解回顾",level:2},{label:"达坦宗旨",level:2},{label:"联系我们",level:2},{label:"推荐阅读",level:2}],n=`<p>极客时间打造的 QCon+案例研习社以案例讲解视频、主题直播、社群交互结合的全新线上学习模式，为数字化人才提供利用碎片化时间来提升知识和技能的平台。</p>
+const e="/zh-cn/assets/image1-d5e6eb74.jpg",t="/zh-cn/assets/image2-5d4b8e33.jpg",s="/zh-cn/assets/image3-a4b62ce6.jpg",a=[e,t,s],p={label:"创造性地精细造车轮：RDMA的Rust安全实践",description:"极客时间打造的QCon+案例研习社以案例讲解视频、主题直播、社群交互结合的全新线上学习模式，为数字化人才提供利用碎片化时间来提升知识和技能的平台",location:"新加坡",date:"2022-07-28",title:"Creatively fine tuned wheels: RDMA's Rust safety practices"},i=[{label:"讲解回顾",level:2},{label:"达坦宗旨",level:2},{label:"联系我们",level:2},{label:"推荐阅读",level:2}],n=`<p>极客时间打造的 QCon+案例研习社以案例讲解视频、主题直播、社群交互结合的全新线上学习模式，为数字化人才提供利用碎片化时间来提升知识和技能的平台。</p>
 <h2 id="讲解回顾">讲解回顾</h2>
 <p>达坦科技联合创始人施继成受邀作为案例研习社的讲师，近日分享了达坦实用的、可落地的 Rust 技术应用开发经验和实践案例，介绍了达坦是如何使用以安全性著称的系统编程语言—Rust 去封装 RDMA 接口以达到安全高效的目的。</p>
-<p><img src="${t}" alt="图片"></p>
+<p><img src="${e}" alt="图片"></p>
 <p>在分享中，施继成简单介绍了 Rust 语言的三大特性。首先，Rust 语言的所有权（ownership）和借用（borrowing）机制对内存管理友好。这一特性非常严格地控制了如果一个内存被大家共享的时候的访问权限和边界。</p>
 <p>其次，Rust 提供了很方便的异步编程接口。它有 Async 和 Await 两个关键词，同时在它的标准库中有 Futures 的完整的接口支持。因此，Rust 天生支持异步 I/O 编程接口。</p>
 <p>最后，Rust 还支持零成本抽象。也就是说，在程序构建的过程中，它能提供一个比较高的抽象层，但是抽象层又不会带来性能损失。这带来两大优势: 第一，在构建程序的时候，得以用到比较高级的抽象层，减少了代码冗余；第二，又不会因为抽象层带来性能的损失，因为编译器已经帮忙将抽象展开。鱼和熊掌，两者兼得。</p>
@@ -13,7 +13,7 @@ const t="/zh-cn/assets/image1-d5e6eb74.jpg",e="/zh-cn/assets/image2-5d4b8e33.jpg
 <p>欲了解此次分享中具体的技术细节和设计要点，请点击以下链接，领取极客时间 7 天会员，免费观看完整视频：<br>
 <a href="https://time.geekbang.org/activity/promo?page_name=page_447">https://time.geekbang.org/activity/promo?page_name=page_447</a></p>
 <p>有意观看完整视频，请扫如下的二维码：</p>
-<p><img src="${e}" alt="图片"></p>
+<p><img src="${t}" alt="图片"></p>
 <h2 id="联系我们">联系我们</h2>
 <p>欢迎参与达坦科技开源项目 async-rdma，Github 链接：<a href="https://github.com/datenlord/async-rdma">https://github.com/datenlord/async-rdma</a></p>
 <p>有意加入 Rust 前沿交流讨论群，请扫如下的二维码</p>
--- zh-cn/assets/index-d83de4f5.js
+++ zh-cn/assets/index-d83de4f5.js
@@ -1,4 +1,4 @@
-const e="/zh-cn/assets/cover-c17c52cc.png",c="/zh-cn/assets/image1-0dff4570.png",s="/zh-cn/assets/image2-e09cfdc8.png",o="/zh-cn/assets/image3-2b589463.png",p="/zh-cn/assets/image4-5841c9d6.png",a="/zh-cn/assets/image5-92344f1d.png",d="/zh-cn/assets/image6-b429f851.png",r="/zh-cn/assets/image7-97920b5a.png",i="/zh-cn/assets/image8-ecf5c6f3.png",t="/zh-cn/assets/image9-f47bad66.png",n="/zh-cn/assets/image10-b50adc27.png",l="/zh-cn/assets/image11-99ec2a51.png",m=[e,c,s,o,p,a,d,r,i,t,n,l],h={label:"从数学角度思考程序与验证正确性",description:"本文旨在为没有接触过形式化方法的读者提供一种新的视角看待计算机系统与算法，而非形式化方法或 TLA+ 教程。因此本文的重点是如何从数学角度思考程序，不会使用大篇幅讲解 TLA+ 的语法。",cover:"./cover.png",location:"中国香港",author:["田野"],tags:["Xline"],date:"2023-03-02",title:"Thinking about programs mathematically and verifying correctness"},g=[{label:"我们该如何写出正确的程序？",level:2},{label:"程序中会有什么样错误？",level:3},{label:"经验",level:3},{label:"形式化方法",level:3},{label:"TLA+",level:2},{label:"简单的例子",level:3},{label:"Two-Phase Commit",level:2},{label:"总结",level:2},{label:"我们的项目：Xline",level:2}],A=`<p><img src="${e}" alt="封面"></p>
+const e="/zh-cn/assets/cover-c17c52cc.png",s="/zh-cn/assets/image1-0dff4570.png",c="/zh-cn/assets/image2-e09cfdc8.png",o="/zh-cn/assets/image3-2b589463.png",a="/zh-cn/assets/image4-5841c9d6.png",p="/zh-cn/assets/image5-92344f1d.png",d="/zh-cn/assets/image6-b429f851.png",t="/zh-cn/assets/image7-97920b5a.png",i="/zh-cn/assets/image8-ecf5c6f3.png",r="/zh-cn/assets/image9-f47bad66.png",n="/zh-cn/assets/image10-b50adc27.png",l="/zh-cn/assets/image11-99ec2a51.png",m=[e,s,c,o,a,p,d,t,i,r,n,l],g={label:"从数学角度思考程序与验证正确性",description:"本文旨在为没有接触过形式化方法的读者提供一种新的视角看待计算机系统与算法，而非形式化方法或 TLA+ 教程。因此本文的重点是如何从数学角度思考程序，不会使用大篇幅讲解 TLA+ 的语法。",cover:"./cover.png",location:"中国香港",author:["田野"],tags:["Xline"],date:"2023-03-02",title:"Thinking about programs mathematically and verifying correctness"},h=[{label:"我们该如何写出正确的程序？",level:2},{label:"程序中会有什么样错误？",level:3},{label:"经验",level:3},{label:"形式化方法",level:3},{label:"TLA+",level:2},{label:"简单的例子",level:3},{label:"Two-Phase Commit",level:2},{label:"总结",level:2},{label:"我们的项目：Xline",level:2}],A=`<p><img src="${e}" alt="封面"></p>
 <p>本文旨在为没有接触过形式化方法的读者提供一种新的视角看待计算机系统与算法，而非形式化方法或 TLA+ 教程。因此本文的重点是如何从数学角度思考程序，不会使用大篇幅讲解 TLA+ 的语法。</p>
 <h2 id="我们该如何写出正确的程序？">我们该如何写出正确的程序？</h2>
 <p>程序设计的目标永远是写出正确的程序。随着时间的推移，我们的程序越来越复杂，其中可能存在的错误也越来越多。想要写出正确的程序，首先应该了解程序中可能出现的错误有哪些。</p>
@@ -60,14 +60,14 @@ const e="/zh-cn/assets/cover-c17c52cc.png",c="/zh-cn/assets/image1-0dff4570.png"
 [pc : start] -> [pc : middle] -> [pc : done]</p>
 <h4>状态编写</h4>
 <p><code>i</code> 的初始值为 0，<code>pc</code> 的初始值为 <code>start</code>，我们可以把次态关系写成：</p>
-<p><img src="${c}" alt="图片"></p>
+<p><img src="${s}" alt="图片"></p>
 <p>其中，对于变量 <code>i</code>，它的下一个状态表示为 <code>i'</code>，这是 TLA+ 中定义变量状态转换的方式。<code>i' ∈ 0..1000</code> 代表 <code>i</code> 在下一个状态的值是 0 到 1000 之间的一个数，也就是 <code>someNumber()</code>，<code>0..1000</code> 代表集合 <code>{0,1,...,1000}</code>。<code>∧</code> 是布尔逻辑中的逻辑与，可以认为意为"并且"。最终程序运行完成，不会再有下一个状态，因此表示为 <code>FALSE</code>。</p>
 <p>在 TLA+ 中，我们编写的是一个个状态。因此，并非是"因为 <code>pc = start</code> 所以 <code>i' ∈ 0..1000</code>"，事实上两者的关系是并列的：<strong>在这个状态中，<code>pc</code> 的值是 <code>start</code> 并且 <code>i</code> 下一个状态的值 <code>∈ 0..1000</code></strong>。有了这样的思想，我们可以将上面的抽象改写为：</p>
-<p><img src="${s}" alt="图片"></p>
+<p><img src="${c}" alt="图片"></p>
 <p>在其中用到了"或"连接两个状态，我们可以用布尔逻辑中的逻辑或 <code>∨</code> 来表示。这样，我们就可以清晰地表示出程序的状态转换关系了。为了美观，在 TLA+ 中，首句前也可以补上相同的布尔逻辑符号：</p>
 <p><img src="${o}" alt="图片"></p>
 <p>我们最终得到了这个简单程序在初始状态后的两个状态，下面我们将初始状态补全，并按照 TLA+ 语言的要求补全整个 specification：</p>
-<p><img src="${p}" alt="图片"></p>
+<p><img src="${a}" alt="图片"></p>
 <ul>
 <li>EXTENDS 用于引入其他 specification 中定义的 module，这里引入了标准库中的 Integers，主要用在 i' ∈ 0..1000 上。</li>
 <li>VARIABLES 用于定义变量，这里定义了 i 和 pc。</li>
@@ -83,7 +83,7 @@ Next 用于定义状态转换关系。</p>
 <p>二阶段提交（英语：Two-phase Commit）是指在计算机网络以及数据库领域内，为了使基于分布式系统架构下的所有节点在进行事务提交时保持一致性而设计的一种算法。通常，二阶段提交也被称为是一种协议（Protocol）。在分布式系统中，每个节点虽然可以知晓自己的操作时成功或者失败，却无法知道其他节点的操作的成功或失败。当一个事务跨越多个节点时，为了保持事务的 ACID 特性，需要引入一个作为协调者的组件来统一掌控所有节点（称作参与者）的操作结果并最终指示这些节点是否要把操作结果进行真正的提交（比如将更新后的数据写入磁盘等等）。因此，二阶段提交的算法思路可以概括为：参与者将操作成败通知协调者，再由协调者根据所有参与者的反馈情报决定各参与者是否要提交操作还是中止操作。—— <strong>Two-Phase Commit (Wikipedia)</strong></p>
 </blockquote>
 <p>在 <strong>Leslie Lamport's The TLA+ Video Course</strong> 中，Lamport 以这样的方式类比解释 Two-Phase Commit：</p>
-<p><img src="${a}" alt="图片"></p>
+<p><img src="${p}" alt="图片"></p>
 <p>在婚礼上，牧师是协调者，新郎和新娘是参与者。当新郎和新娘都同意婚事时，牧师才会正式宣布婚事。如果有一方不同意，牧师就会中止婚事：</p>
 <ol>
 <li>牧师问新郎：你是否同意这件婚事？</li>
@@ -105,7 +105,7 @@ Next 用于定义状态转换关系。</p>
 <li>变量 <code>msgs</code> 作为消息池，用于记录所有正在传输的消息，初始值是一个空集。</li>
 </ul>
 <p>下面我们来定义系统做可能发生的动作。</p>
-<p><img src="${r}" alt="图片"></p>
+<p><img src="${t}" alt="图片"></p>
 <ul>
 <li>TLA+ 中可以用上述方式定义类似于其它编程语言中"函数"概念的表达式，这样就无需对每一个 Resource Manager 都定义一个表达式了。</li>
 <li><code>[type → "prepare", rm → r]</code> 是一个 TLA+ 中的 record，类似于其它编程语言中的 struct。</li>
@@ -114,7 +114,7 @@ Next 用于定义状态转换关系。</p>
 <p>当 <code>TM</code> 的状态为 <code>init</code>，且在消息池中存在来自 <code>r</code> 的 <code>Prepared</code> 消息，<code>tmPrepared</code> 在下一个状态的值会是 <code>tmPrepared</code> 和 <code>{r}</code> 的并集。</p>
 <p><img src="${i}" alt="图片"></p>
 <p>上面的两个动作分别是 Transaction Manager 进行 Commit 与 Abort。</p>
-<p><img src="${t}" alt="图片"></p>
+<p><img src="${r}" alt="图片"></p>
 <p>上述 4 个 Resource Manager 动作分别是 Resource Manager 选择 Prepare 与 Abort，以及处理由 Transaction Manager 决定的 Commit 与 Abort。</p>
 <p>其中，存在语法如 <code>rmState' = [rmState except ![r] = "prepared"]</code>，意为"在下一个状态中，<code>rmState[r]</code> 的值变为 <code>prepared</code>，其它部分不变"。</p>
 <p>如果我们用形如 <code>rmState[r]' = "prepared"</code> 的形式来表示，我们并没有显式地说明 <code>rmState</code> 的其它部分在下一个状态的值，因此是不正确的。</p>
@@ -149,4 +149,4 @@ Next 用于定义状态转换关系。</p>
 <h2 id="我们的项目：xline">我们的项目：Xline</h2>
 <p>TLA+被广泛用于分布式系统算法的研究和开发中。在我们的项目 Xline 中，TLA+被用来在设计阶段验证共识算法的正确性。</p>
 <p>Xline 是一个用于元数据管理的分布式 KV 存储。我们在 Xline 中使用 CURP 协议（<a href="https://www.usenix.org/system/files/nsdi19-park.pdf)%E7%9A%84%E4%BF%AE%E6%94%B9%E7%89%88%E4%BD%9C%E4%B8%BA%E5%85%B1%E8%AF%86%E5%8D%8F%E8%AE%AE%EF%BC%8CTLA+%E5%B0%86%E8%A2%AB%E7%94%A8%E4%BA%8E%E5%85%B6%E6%AD%A3%E7%A1%AE%E6%80%A7%E9%AA%8C%E8%AF%81%E4%B8%AD%E3%80%82">https://www.usenix.org/system/files/nsdi19-park.pdf)的修改版作为共识协议，TLA+将被用于其正确性验证中。</a></p>
-<p>如果你想了解更多关于 Xline 的信息，请参考我们的 Github：<a href="https://github.com/datenlord/Xline">https://github.com/datenlord/Xline</a></p>`;export{m as assetURLs,A as default,h as metadata,g as toc};
+<p>如果你想了解更多关于 Xline 的信息，请参考我们的 Github：<a href="https://github.com/datenlord/Xline">https://github.com/datenlord/Xline</a></p>`;export{m as assetURLs,A as default,g as metadata,h as toc};
