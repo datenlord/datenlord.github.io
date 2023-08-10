21477b4f3cb86acbeb8dba7048a754fa9350592d
LingKa <cnfty786@gmail.com>
2023-08-10 11:17:10 +0800

    feat: add class spinalHDL videos

--- src/pages/TechShare/SpinalHDL.ts
+++ src/pages/TechShare/SpinalHDL.ts
@@ -7,6 +7,9 @@ import cover6 from '@/assets/spinal-hdl/cover6.jpg'
 import cover7 from '@/assets/spinal-hdl/cover7.jpg'
 import cover8 from '@/assets/spinal-hdl/cover8.jpg'
 import cover9 from '@/assets/spinal-hdl/cover9.jpg'
+import cover10 from '@/assets/spinal-hdl/cover10.jpg'
+import cover11 from '@/assets/spinal-hdl/cover11.jpg'
+import cover12 from '@/assets/spinal-hdl/cover12.jpg'
 
 export const spinalHDLItems = [
   {
@@ -80,5 +83,29 @@ export const spinalHDLItems = [
     text: '为了让对开源硬件感兴趣的朋友进一步了解SpinalHDL，达坦科技联合SpinalHDL社区举办一场题为《SpinalHDL应用前景探索》的线上研讨会（Webinar）。本研讨会分三个讨论分主题，分别为：数据通路加速（Datapath/Accelerations）、复杂系统设计案例（Design）、硬件设计流程（Flow）。这是本场研讨会的第九个workshop：From Peripheral Plugins to Product documentation，由Andreas Wallner进行分享。',
     date: '2022.12.21',
     url: 'https://www.bilibili.com/video/BV12v4y1Q7cg/',
+  },
+  {
+    key: 10,
+    image: cover10,
+    title: '如何用Rust写分布式系统',
+    text: '分享会介绍分布式KV Store (Xline) 整体的组成，以及不同组件之间如何去交互。在广域网下的分布式系统如何高效地达成共识，以及分布式系统如何实现容错。',
+    date: '2023.06.24',
+    url: 'https://www.bilibili.com/video/BV1Hs4y1r7dD/',
+  },
+  {
+    key: 11,
+    image: cover11,
+    title: 'Building microsecond-scale system with modern networking in Rust',
+    text: '随着新型网络技术，如RDMA的普及，现代分布式应用的延迟降到了毫秒甚至微秒级别。这为构建系统软件带来了新的挑战。Rust的zero-cost抽象及内存管理能力能够为构建新型分布式应用带来新的机遇。我将分享利用Rust在用户态及内核态构建新型系统软件的相关经验，包括如何利用Rust抽象RDMA硬件的底层抽象及如何在内核构建一个毫秒级的容器启动机制',
+    date: '2023.06.24',
+    url: 'https://www.bilibili.com/video/BV1Zo4y1P7Sg/',
+  },
+  {
+    key: 12,
+    image: cover12,
+    title: '30分钟体验Rust for Linux内核模块开发',
+    text: '本主题会提供构建好的docker开发环境，在30分钟内演示如何使用r4l开发一个简易内核模块，并介绍目前r4l的发展现状。可以自备好电脑现场体验。',
+    date: '2023.06.24',
+    url: 'https://www.bilibili.com/video/BV12j411U78o/',
   },
 ]
