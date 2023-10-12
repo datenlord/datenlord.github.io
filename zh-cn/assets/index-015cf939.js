e7ae3e85ce8ebf9c25fc8c7c541d99a363a762c3
LingKa <cnfty786@gmail.com>
2023-10-12 15:28:16 +0800

    feat: deploy github page

--- zh-cn/assets/index-015cf939.js
+++ zh-cn/assets/index-015cf939.js
@@ -1,4 +1,4 @@
-const n="/zh-cn/assets/cover-bc86e95c.jpg",t="/zh-cn/assets/image1-d4391053.jpg",e=[n,t],o={label:"DatenLord开源产品技术分享 | Xline源码解读",description:"传统单数据中心解决方案无法满足跨数据中心的场景对性能和一致性的需求。DatenLord推出开源分布式KV存储Xline，针对多数据中心场景，可以实现数据的高性能跨云、跨数据中心共享访问，并且保证数据的一致性。本期源码解读将聚焦Xline的Curp共识协议，DatenLord分布式存储工程师赵佳炜将与大家分享在Xline中应用的Curp共识协议的设计与代码实现。",cover:"./cover.jpg",location:"中国香港",author:["赵佳炜"],tags:["Xline"],date:"2023-04-24",title:"DatenLord open source product technology sharing l Xline source code interpretation"},r=[{label:"【源码解读】Xline Curp 共识协议的设计与实现",level:2}],l=`<p><img src="${n}" alt="封面"></p>
+const n="/zh-cn/assets/cover-bc86e95c.jpg",t="/zh-cn/assets/image1-d4391053.jpg",e=[n,t],r={label:"DatenLord开源产品技术分享 | Xline源码解读",description:"传统单数据中心解决方案无法满足跨数据中心的场景对性能和一致性的需求。DatenLord推出开源分布式KV存储Xline，针对多数据中心场景，可以实现数据的高性能跨云、跨数据中心共享访问，并且保证数据的一致性。本期源码解读将聚焦Xline的Curp共识协议，DatenLord分布式存储工程师赵佳炜将与大家分享在Xline中应用的Curp共识协议的设计与代码实现。",cover:"./cover.jpg",location:"中国香港",author:["赵佳炜"],tags:["Xline"],date:"2023-04-24",title:"DatenLord open source product technology sharing l Xline source code interpretation"},o=[{label:"【源码解读】Xline Curp 共识协议的设计与实现",level:2}],l=`<p><img src="${n}" alt="封面"></p>
 <p>传统单数据中心解决方案无法满足跨数据中心的场景对性能和一致性的需求。<strong>DatenLord 推出开源分布式 KV 存储 Xline</strong>，针对多数据中心场景，可以实现数据的高性能跨云、跨数据中心共享访问，并且保证数据的一致性。</p>
 <p>本期源码解读将聚焦 <strong>Xline 的 Curp 共识协议</strong>，DatenLord 分布式存储工程师<strong>赵佳炜</strong>将与大家分享<strong>在 Xline 中应用的 Curp 共识协议的设计与代码实现</strong>。</p>
 <h2 id="【源码解读】xline-curp-共识协议的设计与实现">【源码解读】Xline Curp 共识协议的设计与实现</h2>
@@ -17,4 +17,4 @@ const n="/zh-cn/assets/cover-bc86e95c.jpg",t="/zh-cn/assets/image1-d4391053.jpg"
 <p><strong>【直播预约】</strong>
 欢迎您预约直播，或者登陆腾讯会议观看直播：<br>
 会议号：955-6910-3992</p>
-<p><img src="${t}" alt="图片"></p>`;export{e as assetURLs,l as default,o as metadata,r as toc};
+<p><img src="${t}" alt="图片"></p>`;export{e as assetURLs,l as default,r as metadata,o as toc};
