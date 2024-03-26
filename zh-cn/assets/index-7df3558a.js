85ecfd1c87ebab29eeabf0f3584a30c157434c64
LingKa <cnfty786@gmail.com>
2024-03-27 02:37:51 +0800

    feat: deploy github page

---
+++ zh-cn/assets/index-7df3558a.js
@@ -0,0 +1,14 @@
+const s="/zh-cn/assets/cover-9c53ea5f.png",n="/zh-cn/assets/image1-52cf1c3c.jpg",t="/zh-cn/assets/image1-0a18e154.png",o=[s,n,t],a={label:"SpinalHDL应用前景探索线上研讨会",description:"新一代硬件描述语言SpinalHDL的发明人Charles Papon说，SpinalHDL始于2014年，最初是作为VHDL/Verilog的替代而做的创新尝试，伴随着数年来开源硬件设计的蓬勃发展，基于开源技术的硬件设计方法和范式逐渐受到业界的关注，那么SpinalHDL有哪些最新的进展和落地实践呢？",cover:"./cover.png",location:"中国香港",date:"2022-12-08",title:"SpinalHDL Application Prospect Exploration Online Seminar"},p=[{label:"线上研讨会",level:2}],r=`<p>新一代硬件描述语言 SpinalHDL 的发明人 Charles Papon 说，SpinalHDL 始于 2014 年，最初是作为 VHDL/Verilog 的替代而做的创新尝试，伴随着数年来开源硬件设计的蓬勃发展，基于开源技术的硬件设计方法和范式逐渐受到业界的关注，那么 SpinalHDL 有哪些最新的进展和落地实践呢？</p>
+<h2 id="线上研讨会">线上研讨会</h2>
+<p><strong>达坦科技（DatenLord）</strong> 致力于打造高性能跨云存储，其通过软硬件深度融合的方式打破云之间的壁垒，实现高性能跨云数据访问，提供海量异地、异构数据的统一存储访问机制，为云上应用提供高性能安全存储支持。达坦科技采用硬件加速提升存储性能，目前采用 FPGA、ASIC 实现存储相关场景的性能加速。SpinalHDL 是达坦科技在产品中使用的硬件描述语言之一，达坦科技也一直热心于推广 SpinalHDL 在业界的落地应用。</p>
+<p>为了让对开源硬件感兴趣的朋友进一步了解 SpinalHDL，在北京时间 <strong>2022 年 12 月 16 日下午 17:00-21:00</strong>，达坦科技联合 SpinalHDL 社区举办一场题为《<strong>SpinalHDL 应用前景探索</strong>》的线上研讨会（Webinar），本研讨会分三个讨论分主题，分别为：<strong>数据通路加速</strong>（Datapath/Accelerations）、<strong>复杂系统设计案例</strong>（Design）、<strong>硬件设计流程</strong> （Flow）。</p>
+<p><img src="${n}" alt="图片"></p>
+<p>欢迎所有对开源硬件及 SpinalHDL 感兴趣的朋友，参加我们的<strong>线上研讨会</strong> ，观看分享和在线演示</p>
+<p><strong>主题</strong>：Explore the SpinalHDL Landscape<br>
+<strong>时间</strong>：2022 年 12 月 16 日 17:00 （北京时间）<br>
+<strong>加入 Zoom 会议</strong>：
+<a href="https://zoom.us/j/83066307799?pwd=Z0ZyNWdnaDhMNHNrQWJCWi9OaVpjUT09">https://zoom.us/j/83066307799?pwd=Z0ZyNWdnaDhMNHNrQWJCWi9OaVpjUT09</a><br>
+<strong>会议号</strong>：830 6630 7799<br>
+<strong>密码</strong>：1216</p>
+<p>对开源硬件感兴趣的朋友，可以添加小助手微信，加入<strong>达坦科技硬件群</strong>。</p>
+<p><img src="${t}" alt="图片"></p>`;export{o as assetURLs,r as default,a as metadata,p as toc};
