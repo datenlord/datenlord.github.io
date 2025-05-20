3b781d803c796dc808e0dcc1522db951fb253924
yiboyasss <3359595624@qq.com>
2025-05-20 11:44:38 +0800

    UI: zh-cn

---
+++ zh-cn/assets/index-4d54e141.js
@@ -0,0 +1,23 @@
+const t="/zh-cn/assets/cover-5856255d.png",s="/zh-cn/assets/image1-a156153e.png",o="/zh-cn/assets/image2-b0a4a464.png",n="/zh-cn/assets/image1-bf4f8777.png",r="/zh-cn/assets/image-d0c27c17.png",a=[t,s,o,n,r],e={label:"2024 RISC-V中国峰会同期活动预告 l 硬件敏捷开发与验证方法学研讨会",description:"2024 RISC-V 中国峰会将于8月19日至25日在杭州黄龙饭店隆重举行。峰会采用“主会议+技术研讨会+展览展示+同期活动”的创新形式，将有超过20场同期活动在8月19日至25日期间同步进行。RISC-V 中国峰会至今已成功举办四届，现已成为中国大陆规格最高、规模最大、影响力最强的专业会议之一。作为本届RISC-V中国峰会的同期活动，8月22日上午，达坦科技将举办第三届硬件敏捷开发与验证方法学研讨会。",cover:"./cover.png",location:"新疆",date:"2024-08-16",title:"RISC V China Summit Simultaneous Event Preview l Hardware Agile Development and Verification Methodology Seminar"},g=[{label:"活动信息",level:2}],i=`<p><img src="${t}" alt="图片">
+<strong>2024 RISC-V中国峰会</strong>
+2024 RISC-V 中国峰会将于8月19日至25日在杭州黄龙饭店隆重举行。峰会采用“主会议+技术研讨会+展览展示+同期活动”的创新形式，将有超过20场同期活动在8月19日至25日期间同步进行。
+RISC-V 中国峰会至今已成功举办四届，现已成为中国大陆规格最高、规模最大、影响力最强的专业会议之一。作为本届RISC-V中国峰会的同期活动，8月22日上午，达坦科技将举办第三届硬件敏捷开发与验证方法学研讨会。
+在数字化转型浪潮的推动下，数字芯片作为信息技术的核心载体，其设计与验证的复杂度与日俱增，市场竞争亦趋于白热化。面对这一挑战，如何有效提升数字芯片的开发与验证效率，成为了摆在整个行业面前的重大课题。传统基于Verilog、SystemVerilog、VHDL及UVM的设计与验证方法虽已根深蒂固，但面对新时代的需求，其局限性日益凸显。</p>
+<p>近年来，一股新兴力量正悄然改变着这一现状——以Chisel、SpinalHDL为代表的新一代硬件描述语言（HDL）以及Cocotb、pyuvm等新一代验证框架的崛起，为数字芯片的设计与验证领域注入了新的活力。这些新兴技术不仅在语法表达、代码简洁性及错误检查等方面展现出显著优势，更重要的是，它们为硬件敏捷设计与验证开辟了全新的路径，为业界带来了前所未有的灵活性和效率提升。</p>
+<p>为了深入探讨这一领域的最新进展，促进技术交流与创新，本次研讨会特邀来自顶尖学术机构与科研前沿的几位专家，共同分享他们在硬件敏捷设计与验证方面的实践经验与前沿思考
+<img src="${s}" alt="图片"></p>
+<h2 id="活动信息">活动信息</h2>
+<p><strong>活动时间：</strong> 2024年8月22日，上午9:30-11:45</p>
+<p><strong>活动地点</strong>：杭州黄龙饭店 珍珠厅1</p>
+<p><strong>观众报名链接：</strong>
+<img src="${o}" alt="图片">
+<strong>第二届硬件敏捷开发与方法学研讨会 l 2023 RISC-V中国峰会同期活动顺利举办</strong>
+<img src="${n}" alt="图片">
+<strong>达坦科技</strong>始终致力于打造高性能 <strong>Al+ Cloud 基础设施平台</strong>，积极推动 AI 应用的落地。达坦科技通过<strong>软硬件深度融合</strong>的方式，提供高性能存储和高性能网络。为 AI 应用提供<strong>弹性、便利、经济</strong>的基础设施服务，以此满足不同行业客户对 AI+Cloud 的需求。</p>
+<p><strong>公众号：</strong> 达坦科技DatenLord</p>
+<p><strong>DatenLord官网：</strong> <a href="https://datenlord.github.io/zh-cn/">https://datenlord.github.io/zh-cn/</a></p>
+<p><strong>知乎账号：</strong> <a href="https://www.zhihu.com/org/da-tan-ke-ji">https://www.zhihu.com/org/da-tan-ke-ji</a></p>
+<p><strong>B站：</strong> <a href="https://space.bilibili.com/2017027518">https://space.bilibili.com/2017027518</a></p>
+<p><strong>邮箱：</strong> <a href="mailto:info@datenlord.com">info@datenlord.com</a></p>
+<p>如果您有兴趣加入<strong>达坦科技Rust前沿技术交流群或硬件相关的群</strong>  ，请添加<strong>小助手微信</strong>：DatenLord_Tech
+<img src="${r}" alt="图片"></p>`;export{a as assetURLs,i as default,e as metadata,g as toc};
---
+++ zh-cn/assets/index-6be49487.js
@@ -0,0 +1,132 @@
+const e="/zh-cn/assets/image1-36c2f038.png",o=[e],c={label:"cocotbext-pcie使用经验交流",description:"cocotbext-pcie是Alex Forencich基于cocotb开发的一套PCIe仿真框架，其原型诞生于Alex Forencic的另一个知名开源网卡项目corundum，由于corundum中大量使用了PCIe接口进行数据传输，为了对相关功能进行仿真，Alex基于ccootb编写了一套仿真环境，这便是cocotbext-pcie的前身。后来，相关的测试框架代码从corundum的代码库中独立出来，成为了现在的cocotbext-pcie项目。",location:"中国香港",cover:"./image1.png",author:["米明恒"],date:"2025-05-09",title:"Experience Sharing on Using cocotbext pcie"},p=[{label:"写在前面",level:2},{label:"cocotbext-pcie项目的介绍",level:2},{label:"cocotbext-pcie的简单使用方式",level:2},{label:"目前cocotbext-pcie所存在的一些缺陷",level:2},{label:"达坦科技的开源夏令营计划",level:2}],t=`<p><img src="${e}" alt="图片"></p>
+<h2 id="写在前面">写在前面</h2>
+<p>达坦科技即将开源的100G RDMA RTL代码采用cocotb对其功能进行验证。其中，对于DMA引擎的验证工作使用了开源的cocotbext-pcie框架来进行验证。本文将简要介绍cocotbext-pcie的使用方式，以及在使用中遇到的问题。</p>
+<p>对于目前遇到的一些问题，由于需要对cocotbext-pcie本身进行修改，达坦科技计划在暑期安排一系列夏令营活动，邀请在校生加入对cocotbext-pcie的完善工作，具体活动信息将在近期公布。</p>
+<p>关于cocotb的话题，会有一系列文章进行讨论，本篇是此系列的第三篇，欢迎大家关注达坦科技公众号，第一时间阅读最新文章。</p>
+<h2 id="cocotbext-pcie项目的介绍">cocotbext-pcie项目的介绍</h2>
+<p>cocotbext-pcie是Alex Forencich基于cocotb开发的一套PCIe仿真框架，其原型诞生于Alex Forencic的另一个知名开源网卡项目corundum，由于corundum中大量使用了PCIe接口进行数据传输，为了对相关功能进行仿真，Alex基于ccootb编写了一套仿真环境，这便是cocotbext-pcie的前身。后来，相关的测试框架代码从corundum的代码库中独立出来，成为了现在的cocotbext-pcie项目。</p>
+<p>由于该项目中对PCIe的链路层通信过程进行了非常细致的建模，对于刚接触PCIe协议的初学者而言，在阅读PCIe标准spec的同时，如果能够对照着该项目的Python代码进行对照学习，对于加深对协议的理解会有非常大的帮助。为了帮助读者快速熟悉并阅读项目的代码，下面来介绍一下该项目的代码结构，如下所示。</p>
+<pre><code>cocotbext
+└── pcie
+    ├── core
+    │   ├── bridge.py
+    │   ├── caps
+    │   │   ├── aer.py
+    │   │   ├── common.py
+    │   │   ├── __init__.py
+    │   │   ├── msi.py
+    │   │   ├── pcie.py
+    │   │   ├── pm.py
+    │   ├── device.py
+    │   ├── dllp.py
+    │   ├── endpoint.py
+    │   ├── function.py
+    │   ├── __init__.py
+    │   ├── msi.py
+    │   ├── pci.py
+    │   ├── port.py
+    │   ├── rc.py
+    │   ├── region.py
+    │   ├── switch.py
+    │   ├── tlp.py
+    │   ├── utils.py
+    │   └── version.py
+    ├── intel
+    │   ├── ptile
+    │   │   ├── __init__.py
+    │   │   ├── interface.py
+    │   │   └── ptile_model.py
+    │   └── s10
+    └── xilinx
+        └── us
+</code></pre>
+<p>从代码的组织结构来看，该项目的代码结构非常清晰易懂，其中core目录为仿真框架的主体部分，而另外的两个目录分别是intel和xilinx两家公司FPGA PCIe IP核的仿真模型。</p>
+<p>仿真框架核心</p>
+<p>通过仔细观察core目录，可以看到其中包含了用于构建整个PCIe仿真环境的各个组件，通过这些组件，可以非常方便的搭建起一个从RootComplex到FPGA上硬核之间的完整PCIe数据链路，可以支持PCIe设备的上电枚举、Bar空间地址分配、基于Credit机制的流量控制、数据读写请求等等功能的仿真。可以看到，该目录下的文件均与PCIe标准中所定义的内容对应，与具体厂商的硬核实现无关。</p>
+<p>下面介绍各几个比较重要的文件：</p>
+<p>tlp.py：该文件中定义了TLP的数据格式，以及对TLP的各种操作方法。其中定义的Tlp类是用于连接各个厂商具体PCIe硬核模型的重要桥梁。阅读其代码有助于理解TLP数据包的组装与解析。</p>
+<p>region.py：该文件中实现了对一段内存地址的建模，通过该文件所提供的接口，开发者可以模拟PCIe系统中对main memoey的各种操作。</p>
+<p>rc.py：该文件提供了对RootComplex的仿真模型，实现了诸如设备枚举等功能的仿真。该模块在实例化的过程中有非常多的初始化参数可以配置，为了了解这些参数的作用，阅读该文件的实现是一个很好的途径。在编写测试脚本的过程中，需要实例化一个RootComplex对象，并将一个特定厂商的Device实现连接到RootComplex上。</p>
+<p>dllp.py：该文件描述了DLLP相关的功能，阅读该文件对理解PCIe协议中基于credit的流控机制有很好的帮助。</p>
+<p>function.py：该文件中定义的Function类型是众多其他类型的基类，实现了对于PCIe设备配置空间的行为建模，阅读该文件有助于理解PCIe设备配置空间的相关内容。</p>
+<p>厂商器件支持</p>
+<p>再来看另外两个目录，以intel/ptile目录为例，这是对于Intel的PTile硬核所建立的仿真模型。其中interface.py里面定义了与user logic之间的接口，而ptile_model.py里面则定义了PTile硬核的行为，该文件的重要功能之一就是将上文中tlp.py中所定义的抽象TLP数据结构转换为厂商私有的格式，再通过interface.py与用户编写的DUT模块相连接。</p>
+<p>可以看到，目前项目中支持的PCIe硬核只有Intel和Xilinx两家公司的产品，而且型号仅限于4条产品线。达坦科技的400G RDMA网卡方案使用了Intel的RTile硬核来提供PCIe Gen5接口，而目前cocotbext-pcie内置的模型仅支持到PTile，为此，达坦科技尝试为其编写了RTile的器件适配代码；受限于篇幅，将在另一篇文章中展开介绍如何实现对新器件的适配。对于感兴趣的同学，可以先阅读仍处于开发测试阶段的开源代码，待验证稳定后会向上游发起合并：</p>
+<p><a href="https://github.com/myrfy001/cocotbext-pcie">https://github.com/myrfy001/cocotbext-pcie</a></p>
+<h2 id="cocotbext-pcie的简单使用方式">cocotbext-pcie的简单使用方式</h2>
+<p>通常，基于该框架开发的测试脚本，需要包括以下准备工作：</p>
+<p>实例化一个RootComplex对象，作为主机侧的控制入口；实例化一个特定厂商的PCIe硬核模型对象，提供与DUT对接的接口。</p>
+<p>配置内存地址相关的参数，对于RootComplex对象而言，需要为其主备一块用于模拟main memory的内存区域，对于厂商PCIe硬核而言，需要为其配置好BAR地址区间。</p>
+<p>将DUT的接口与PCIe硬核模型接口进行对接。</p>
+<p>将PCIe硬核对象与RootComplex对象连接，并启动enumerate过程。</p>
+<p>通过BDF查找到需要操作的device，并对其进行下一步操作</p>
+<p>经过上述步骤后，就已经准备好了测试前的基础工作了。接下来可以进行两方面的测试工作：</p>
+<p>对BAR内存地址范围内的数据进行读写，模拟CPU侧向device侧发起request，检查device的response。</p>
+<p>从device直接发起DMA请求，对main memory进行读写操作，然后检查main memory的值是否符合预期。</p>
+<p>基于上述流程，给出如下一段示例代码，展示基本的使用步骤：</p>
+<pre><code>from cocotbext.pcie.core import RootComplex
+from cocotbext.pcie.xilinx.us import UltraScalePlusPcieDevice
+from cocotbext.axi import AxiStreamBus
+
+# 实例化RootComplex实例
+rc = RootComplex()
+rc.max_payload_size = 1
+rc.max_read_request_size = 2
+
+# 实例化某厂商的PCIe硬核模型
+dev_model = UltraScalePlusPcieDevice(
+    # 将dut与PCIe硬核模型通过AXI总线进行连接
+    rq_bus=AxiStreamBus.from_prefix(dut, "m_axis_rq"),
+    rc_bus=AxiStreamBus.from_prefix(dut, "s_axis_rc"),
+    cq_bus=AxiStreamBus.from_prefix(dut, "s_axis_cq"),
+    cc_bus=AxiStreamBus.from_prefix(dut, "m_axis_cc"),
+    # 省略若干参数...
+)
+
+# 在RootComplex侧模拟出一块大小为2MB的Main Memory
+main_mem = rc.mem_pool.alloc_region(2*1024*1024)
+
+# 为硬核模型的function 0添加两个BAR地址空间
+dev_model.functions[0].configure_bar(0, 16*1024*1024)
+dev_model.functions[0].configure_bar(1, 16*1024)
+
+# 连接设备和RootComplex(模拟把卡插到PCIe插槽上)
+root_port = rc.make_port()
+root_port.connect(dev_model)
+
+# 执行PCIe的枚举过程，由于枚举过程涉及到实际的TLP传输，会推进仿真时间，需要await
+await tb.rc.enumerate()
+
+# 通过BDF找到要操作的具体的device，注意这里的device和上面的PCIe硬核模型是不一样的
+dev = rc.find_device(dev_model.functions[0].pcie_id)
+
+# 对设备进行配置，需要await
+await dev.enable_device()
+await dev.set_master()
+
+# 环境初始化完成，下面进行实际的功能验证。
+# 下列代码演示了一个通过写入CSR启动DMA传输的例子，展示了如何仿真CPU发起BAR空间访问和设备发起DMA访问。
+
+# 初始化main memory
+main_mem[0] = 0xab
+main_mem[1] = 0x00
+
+# 获取设备BAR的handle，模拟host侧对device BAR的访问
+dev_bar1 = dev.bar_window[1]
+# 向BAR空间地址0x100的位置写入Dword数字1，触发硬件DMA传输
+await dev_bar1.write(0x100, (1).to_bytes(4, byteorder='little', signed=False))
+
+# 等待DMA传输完成
+await Timer(5000, "ns")
+
+# 检查DMA引擎是否工作正常
+assert main_mem[1] == 0xab
+</code></pre>
+<h2 id="目前cocotbext-pcie所存在的一些缺陷">目前cocotbext-pcie所存在的一些缺陷</h2>
+<p>虽然该开源项目为PCIe相关的应用提供了一个简单易用的仿真验证手段，但由于该开源项目脱胎于开源网卡项目，其仿真模型主要覆盖了实现一个普通网卡设备所需要的PCIe功能，故而对PCIe规范中的功能支持并不是完整的，使用过程中仍有一些坑需要避免。以下是达坦科技在使用过程中遇到的一些问题：</p>
+<p>不支持原子操作类型，无法用于验证原子操作的正确性。</p>
+<p>不支持TLP中AT字段非零的情况。</p>
+<p>无法模拟RootComplex侧的访存延迟。</p>
+<p>支持的PCIe硬核模型种类较少。</p>
+<h2 id="达坦科技的开源夏令营计划">达坦科技的开源夏令营计划</h2>
+<p>针对上述存在的问题，达坦科技希望召集对cocotb验证框架感兴趣的同学，利用暑假实践，参与到达坦科技的开源夏令营活动中，为cocotbext-pcie完善功能，回馈开源社区，同时获取达坦的奖金和实习机会。活动详情会在近期公布，欢迎大家关注达坦科技的微信公众号以获取最新消息。</p>`;export{o as assetURLs,t as default,c as metadata,p as toc};
