1dc36c89907fbe294205d9db486d51f96e5c1627
yiboyasss <3359595624@qq.com>
2025-07-31 15:09:26 +0800

    fix: zh-cn

---
+++ zh-cn/assets/index-762542ed.js
@@ -0,0 +1,91 @@
+const p="/zh-cn/assets/image3-e7a83351.png",e="/zh-cn/assets/image1-6caf9701.png",l="/zh-cn/assets/image2-d15c91e1.png",t="/zh-cn/assets/image4-d81e7808.png",s="/zh-cn/assets/image5-e5e2653f.png",a="/zh-cn/assets/image6-a1ac5aed.png",n="/zh-cn/assets/image7-638b9fb3.png",i="/zh-cn/assets/image8-2ec67807.png",c="/zh-cn/assets/image9-19a1cf33.png",g="/zh-cn/assets/image10-0e887e18.png",C="/zh-cn/assets/image-3a1a0ef6.png",h=[p,e,l,t,s,a,n,i,c,g,C],o={label:"【转发】GLCC学生报名延期！速来抢位！",description:"达坦科技此次参与GitLink“确实开源”编程夏令营(GLCC)，我们的开源项目题目为：为开源RDMA网卡编写测试工具。课题简介：为了对RDMA网卡进行功能测试，需要在line-rate的条件下创建出数据包乱序、丢包、重复等情况。目前已有的解决方案大多需要昂贵的P4交换机来实现。本项目要求使用HDL语言在FPGA上实现指定模式的数据包收发功能。",cover:"./image3.png",location:"中国香港",date:"2024-06-27",title:"GLCC Student Registration Extended! Hurry Up and Secure Your Spot!"},r=[{label:"报名方式",level:2},{label:"关于GLCC",level:2},{label:"为什么加入GLCC",level:2},{label:"01深入开源实践",level:3},{label:"02灵活远程实习",level:3},{label:"03导师一对一指导",level:3},{label:"04拓宽人脉网络",level:3},{label:"05丰厚奖励与认证",level:3},{label:"06名企实习机会",level:3},{label:"参与社区",level:2},{label:"GLCC学生时刻表",level:2},{label:"高校学生如何参与",level:2},{label:"01报名时间：",level:3},{label:"02申请步骤：",level:3},{label:"03注意事项：",level:3},{label:"04学生申请指南：",level:3},{label:"协同举办体系",level:2}],d=`<p>达坦科技始终致力于打造高性能 Al+ Cloud 基础设施平台，积极推动 AI 应用的落地。达坦科技通过软硬件深度融合的方式，提供高性能存储和高性能网络。为 AI 应用提供弹性、便利、经济的基础设施服务，以此满足不同行业客户对 AI+Cloud 的需求。</p>
+<p>达坦科技此次参与GitLink“确实开源”编程夏令营(GLCC)，我们的开源项目题目为：为开源RDMA网卡编写测试工具。</p>
+<p>课题简介：为了对RDMA网卡进行功能测试，需要在line-rate的条件下创建出数据包乱序、丢包、重复等情况。目前已有的解决方案大多需要昂贵的P4交换机来实现。本项目要求使用HDL语言在FPGA上实现指定模式的数据包收发功能。</p>
+<p>课题链接：<a href="https://www.gitlink.org.cn/datenlord/open-rdma/issues/1">https://www.gitlink.org.cn/datenlord/open-rdma/issues/1</a></p>
+<p>导师信息：米明恒</p>
+<p>联络方式：<a href="mailto:mingheng.mi@datenlord.com">mingheng.mi@datenlord.com</a></p>
+<p><img src="${e}" alt="图片"></p>
+<h2 id="报名方式">报名方式</h2>
+<p>现在，让我们再来复习一下报名的细节：</p>
+<p>报名截止时间：</p>
+<p>2024年7月4日17:00，别错过哦！</p>
+<p>报名方式：</p>
+<p>1.登录GLCC官网（GitLink确实开源）查看课题列表：</p>
+<p><a href="https://www.gitlink.org.cn/GLCC">https://www.gitlink.org.cn/GLCC</a></p>
+<ol start="2">
+<li>
+<p>点击最下方【阅读原文】即可跳转官网</p>
+</li>
+<li>
+<p>扫码进入官网报名
+<img src="${l}" alt="图片"></p>
+</li>
+</ol>
+<h2 id="关于glcc">关于GLCC</h2>
+<p>第三届GitLink“确实开源”编程夏令营（GLCC）是由中国计算机学会（CCF）主办，CCF 开源发展委员会与之江实验室承办的面向全国高校学生的暑期编程活动。GLCC致力于激励青年学子通过参与真实的开源软件开发实践，锤炼自身技术实力，为开源社区注入新鲜血液，共同推动国内开源社区的蓬勃发展。
+<img src="${p}" alt="图片">
+第三届GLCC将于活动官网（GitLink |确实开源）和公众号（CCF 开源发展委员会）陆续发布入选课题介绍，敬请期待！
+<img src="${t}" alt="图片">
+扫码关注公众号获取GLCC最新内容</p>
+<h2 id="为什么加入glcc">为什么加入GLCC</h2>
+<h3 id="01深入开源实践">01深入开源实践</h3>
+<p>直接投身开源项目，深入了解社区运作，提升代码编写与协作能力。</p>
+<h3 id="02灵活远程实习">02灵活远程实习</h3>
+<p>提供远程实习机会，让你能自由安排工作时间，兼顾学业与职业发展。</p>
+<h3 id="03导师一对一指导">03导师一对一指导</h3>
+<p>每位学生配备资深导师，全程一对一指导支持和经验分享。</p>
+<h3 id="04拓宽人脉网络">04拓宽人脉网络</h3>
+<p>与志同道合的伙伴以及开源社区技术大咖深入交流，拓宽职业视野。</p>
+<h3 id="05丰厚奖励与认证">05丰厚奖励与认证</h3>
+<p>完成开源任务即可获得税前6000至12000元不等的奖金及结项证书，还有机会获得赛事周边与项目认证等。</p>
+<h3 id="06名企实习机会">06名企实习机会</h3>
+<p>表现优秀的同学将有机会进入知名企业实习，为职业生涯开启新篇章。
+众多开源社区参加GLCC</p>
+<p>目前GLCC已经进入学生申请题目阶段，覆盖大模型、人工智能、操作系统、云原生、编程语言、大数据、运维监控、系统工具等30多个技术领域，课题难度分级，等你来挑选。</p>
+<h2 id="参与社区">参与社区</h2>
+<p><img src="${s}" alt="图片"></p>
+<h2 id="glcc学生时刻表">GLCC学生时刻表</h2>
+<p><strong>1. 5月31日-7月4日</strong></p>
+<p>学生报名，提交proposal。</p>
+<p><strong>2. 7月8日</strong></p>
+<p>公示入选学生名单。</p>
+<p>（7月6日进行二次报名，第一次落选的同学不要灰心哦）</p>
+<p><strong>3.  7月9日-8月18日</strong></p>
+<p>项目开发第一阶段。</p>
+<p><strong>4. 8月30日</strong></p>
+<p>项目中期考核，公示考核结果。</p>
+<p><strong>5.  8月31日-10月20日</strong></p>
+<p>项目开发第二阶段。</p>
+<p><strong>6.  10月31日</strong></p>
+<p>结项审核，公示考核结果。</p>
+<p><strong>7.  11月初</strong></p>
+<p>公布优秀项目/学生颁奖典礼。</p>
+<h2 id="高校学生如何参与">高校学生如何参与</h2>
+<h3 id="01报名时间：">01报名时间：</h3>
+<p>2024.5.31-2024.7.4</p>
+<h3 id="02申请步骤：">02申请步骤：</h3>
+<p><img src="${a}" alt="图片">
+<a href="http://www.gitlink.org.cn/glcc/2024/subjects">www.gitlink.org.cn/glcc/2024/subjects</a>
+同学们根据个人专业情况，技术爱好以及所选开源组织的技术类型和参与门槛及奖金等情况筛选出自己喜欢的课题；
+<img src="${n}" alt="图片">
+②点击“课题列表”中每个课题后的“申请课题”，进入报名信息页面填写“个人基本信息”和“课题申请书”，并提交;
+<img src="${i}" alt="图片">
+③组委会收到申请后，审核通过后推荐给该课题所属项目导师，提交后的信息可再次更新。建议同学们同时也发送邮件跟导师联系；</p>
+<p>④加入GLCC官方QQ交流群，或发邮件到<a href="mailto:glcc@ccf.org.cn">glcc@ccf.org.cn</a>，关注活动进展和咨询；
+<img src="${c}" alt="图片">
+扫码加入GLCC官方QQ交流群
+⑤7月8日公布学生中选情况并通过邮件通知，也可再次登陆官网查看。</p>
+<h3 id="03注意事项：">03注意事项：</h3>
+<p>①GLCC面向18岁以上的在校学生，请诚实提交在校学生证明材料组委会会对提交材料进行审核；</p>
+<p>②申请阶段，GLCC允许每个学生同时提交最多2个课题申请。申请结束后，经过与课题导师双向沟通，每个学生最终只能选择完成一个课题；</p>
+<p>③GLCC的每个课题都公布了导师的姓名/ID和邮箱地址，建议提前通过邮件方式和导师建立联系，反复沟通后再提交申请材料，你有充足的时间去了解你将要承担的课题；</p>
+<p>④ 建议在截止日期前三天完成提交，避免因最后大量学生提交申请导致网络拥堵；</p>
+<p>⑤CCF中国计算机学会学生会员申请优先推荐。</p>
+<h3 id="04学生申请指南：">04学生申请指南：</h3>
+<p>GLCC为同学们申请过程中遇到的问题提供了丰富的辅导文档，可在官网查看“帮助文档-学生指南”获取。</p>
+<p>官网链接：</p>
+<p><a href="https://www.gitlink.org.cn/glcc/help">https://www.gitlink.org.cn/glcc/help</a>
+<img src="${g}" alt="图片">
+扫码进入【学生指南】页面</p>
+<h2 id="协同举办体系">协同举办体系</h2>
+<p><img src="${C}" alt="图片"></p>`;export{h as assetURLs,d as default,o as metadata,r as toc};
